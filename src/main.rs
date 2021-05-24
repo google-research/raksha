8942090e93f432847a62de22755bc7cf4f730868
Andrew Ferraiuolo <aferr@google.com>
2021-05-24 15:33:34 +0100

    IRs for describing application graphs.
    Here there are two files for two different successively lower graphs:
    - graph_ir: this would be just below Arcs manifests and includes nested
      structs as potential inputs/outputs to graph nodes.
    - low_graph_ir: this is a simplification of graph_ir that flattens
      structs and separates edges into lists of inputs/outputs.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 // try blocks are used in antlr4-rust
 #![feature(try_blocks)]
 
+// This may be refactored into two crates
+
+// (1) These are about authorization logic
 mod parsing;
 mod ast;
-mod datalog_ir;
 mod souffle_emitter;
+mod datalog_ir;
 mod lowering_ast_datalog;
 mod souffle_interface;
 mod testing_queries;
 
+// (2) these are about graphs
+mod graph_ir;
+mod low_graph_ir;
+
+// the top level parts might connect them
+
 fn main() {
-    // The main function is now zenfully nothing.
+    // At the moment, the main function does nothing. See 
+    // testing_queries to understand how this works
 }
