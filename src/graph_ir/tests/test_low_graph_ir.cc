a824f609fee93075e5e0622d81889239ac06ac3f
Andrew Ferraiuolo <aferr@google.com>
2021-07-26 14:58:12 +0100

    Low graph IR

---
+++ src/graph_ir/tests/test_low_graph_ir.cc
@@ -0,0 +1,19 @@
+#include "low_graph_ir.h"
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    LowGraphAccessPath* p_a = new LowGraphAccessPath("a");
+    LowGraphAccessPath* p_b = new LowGraphAccessPath("b");
+    LowGraphAccessPath* p_c = new LowGraphAccessPath("c");
+    vector<LowGraphAccessPath> inputs = {*p_a, *p_b};
+    vector<LowGraphAccessPath> outputs = {*p_c};
+    LowGraphNode* g = new LowGraphNode("test_node", inputs, outputs);
+    cout << g->PrettyPrint() << endl;
+
+    return 0;
+}
