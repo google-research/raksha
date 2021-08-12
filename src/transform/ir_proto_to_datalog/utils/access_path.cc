e950b4f9a77c43797d3d901d621bd19cf26b8bee
Mark W Winterrowd <winterrowd@google.com>
2021-08-12 10:47:01 -0700

    Add the AccessPath, used to represent paths while converting to Datalog.
    The AccessPath class represents a descent through the substructures of
    an Arcs Manifest, and is used for representing such paths when drawing
    edges and deciding which edges to draw (by, for instance, checking which
    partial AccessPaths match each other in ParticleSpec connections).
    
    The AccessPath is designed to be built bottom up. As such, the order of
    components are reversed internally, allowing for pushing on parent
    components as an access path is returned from leaf fields towards the
    containing Recipe.

---
+++ src/transform/ir_proto_to_datalog/utils/access_path.cc
@@ -0,0 +1,96 @@
+#include "access_path.h"
+
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/match.h"
+#include "absl/strings/string_view.h"
+#include "absl/strings/str_join.h"
+#include "absl/strings/str_cat.h"
+
+#include <string>
+
+namespace raksha::transform::utils {
+
+// Check the components of the two access paths for equality. First, ensure
+// that they have the same number of components, then check component by
+// component.
+bool AccessPath::operator==(const AccessPath &other) const {
+  if (reverse_components_.size() != other.reverse_components_.size()) {
+    return false;
+  }
+
+  for (uint64_t idx = 0; idx < reverse_components_.size(); ++idx) {
+    if (reverse_components_.at(idx) != other.reverse_components_.at(idx)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// We create the AccessPath string by just joining the contents of the
+// component vector in reverse, using '.' as a separator.
+std::string AccessPath::to_string() const {
+  return absl::StrJoin(
+      reverse_components_.rbegin(),
+      reverse_components_.rend(),
+      ".");
+}
+
+// Check that the component is nonempty and does not contain a '.'. Both of
+// these cases seem malformed and may indicate confusion in deserializing the
+// Manifest protos.
+absl::Status AccessPath::check_component_allowed(
+    const absl::string_view component) {
+  if (component.empty()) {
+    return absl::InvalidArgumentError(
+        "Empty component name not allowed in AccessPath.");
+  }
+
+  if (absl::StrContains(component, '.')) {
+    const std::string error_message =
+        absl::StrCat(
+        "Access path component: ",
+        component,
+        " contains a '.' and thus is compound; only simple component names "
+        "are allowed.");
+    return absl::InvalidArgumentError(error_message);
+  }
+  return absl::OkStatus();
+}
+
+// Check that the component is allowed, and if so, create a new AccessPath
+// having it as a leaf component.
+absl::StatusOr<AccessPath> AccessPath::create(
+    const absl::string_view component) {
+  const absl::Status component_allowed_status =
+      check_component_allowed(component);
+  if (!component_allowed_status.ok()) {
+    return component_allowed_status;
+  }
+
+  return AccessPath(component);
+}
+
+// Create a parent access path by creating a copy with the parent_component
+// added above all components of the passed-in access_path. Although this
+// makes a copy, it is built with copy-elision in mind; it is expected that
+// the passed-in access_path will usually no longer be used after being
+// passed into this factory. If that is the case, then add_parent_component
+// will just push the new component onto the end of the existing access_path,
+// making this very efficient.
+absl::StatusOr<AccessPath> AccessPath::create_parent(
+    absl::string_view parent_component,
+    AccessPath access_path) {
+  const absl::Status component_allowed_status =
+      check_component_allowed(parent_component);
+  if (!component_allowed_status.ok()) {
+    return component_allowed_status;
+  }
+
+  access_path.reverse_components_.push_back(parent_component);
+  return access_path;
+}
+
+
+}  // namespace raksha::transform::utils
