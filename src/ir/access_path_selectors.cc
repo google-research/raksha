2803331654a7ed4fa51de0a63d5ca253c85a62c0
Mark W Winterrowd <winterrowd@google.com>
2021-08-18 12:14:38 -0700

    Simplify AccessPathSelectors::ToString using a Formatter.

--- src/ir/access_path_selectors.cc
+++ src/ir/access_path_selectors.cc
@@ -7,17 +7,22 @@
 
 namespace raksha::ir {
 
+// A helper formatter that just calls ToString on a Selector. Used to make
+// AccessPathSelectors::ToString simpler.
+struct SelectorToStringFormatter {
+  void operator()(std::string *out, const Selector &selector) const {
+    out->append(selector.ToString());
+  }
+};
+
 // We create the AccessPathSelectors string by just joining the contents of the
 // component vector in reverse, using '.' as a separator.
 std::string AccessPathSelectors::ToString() const {
-  std::vector<std::string> reverse_selector_strs;
-  for (const Selector &selector : reverse_selectors_) {
-    reverse_selector_strs.push_back(selector.ToString());
-  }
   return absl::StrJoin(
-      reverse_selector_strs.rbegin(),
-      reverse_selector_strs.rend(),
-      "");
+      reverse_selectors_.rbegin(),
+      reverse_selectors_.rend(),
+      "",
+      SelectorToStringFormatter());
 }
 
 }  // namespace raksha::ir
