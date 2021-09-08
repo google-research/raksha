1609e839e24cfd775c64f57528710ac05c2639f8
Mark W Winterrowd <winterrowd@google.com>
2021-09-08 10:49:56 -0700

    Improve comments and formatting in CreateFromManifestProto.

--- src/xform_to_datalog/datalog_facts.cc
+++ src/xform_to_datalog/datalog_facts.cc
@@ -33,9 +33,14 @@ namespace amt = raksha::xform_to_datalog::arcs_manifest_tree;
 //  break this up.
 DatalogFacts DatalogFacts::CreateFromManifestProto(
     const arcs::ManifestProto &manifest_proto) {
+  // These collections will be used as inputs to the constructor that we
+  // return from this function.
   std::vector<ir::TagClaim> result_claims;
   std::vector<ir::TagCheck> result_checks;
   std::vector<ir::Edge> result_edges;
+
+  // Turn each ParticleSpecProto indicated in the manifest_proto into a
+  // ParticleSpec object, which we can use directly.
   absl::flat_hash_map<std::string, amt::ParticleSpec> particle_specs;
   for (const arcs::ParticleSpecProto &particle_spec_proto :
     manifest_proto.particle_specs()) {
@@ -43,10 +48,21 @@ DatalogFacts DatalogFacts::CreateFromManifestProto(
         amt::ParticleSpec::CreateFromProto(particle_spec_proto);
     std::string particle_spec_name = particle_spec.name();
     auto ins_res = particle_specs.insert({
-      std::move(particle_spec_name), std::move (particle_spec) });
+      std::move(particle_spec_name), std::move(particle_spec) });
     CHECK(ins_res.second) << "Name collision on particle spec.";
   }
 
+  // This loop looks at each recipe in the manifest proto and instantiates
+  // the ParticleSpecs indicated by the ParticleProtos in that recipe. It
+  // additionally generates the edges in and out of the Particle to Handles
+  // as indicated by the HandleConnections on the Particles.
+  //
+  // To allow recipes to be distinguished as a component of an AccessPath, we
+  // provide each recipe a unique name. For recipes that have a name
+  // provided by the user, we use the user-provided name. For those recipes
+  // where the user did not provide a name, we generate a name by combining the
+  // generated_recipe_name_prefix and a recipe_num counter that we increment
+  // for each generated recipe name.
   const std::string generated_recipe_name_prefix = "GENERATED_RECIPE_NAME";
   uint64_t recipe_num = 0;
   for (const arcs::RecipeProto &recipe_proto : manifest_proto.recipes()) {
@@ -55,6 +71,10 @@ DatalogFacts DatalogFacts::CreateFromManifestProto(
           ? absl::StrCat(generated_recipe_name_prefix, recipe_num++)
           : recipe_proto.name();
 
+    // For each Particle, generate the relevant facts for that particle.
+    // These fall into two categories: facts that lie within the ParticleSpec
+    // that just need to be instantiated for this particular Particle, and
+    // edges that connect this Particle to input/output Handles.
     uint64_t particle_num = 0;
     for (const arcs::ParticleProto &particle_proto : recipe_proto.particles()) {
       const std::string &particle_spec_name = particle_proto.spec_name();
@@ -63,6 +83,16 @@ DatalogFacts DatalogFacts::CreateFromManifestProto(
       const std::string particle_name =
           absl::StrCat(particle_spec_name, "#", particle_num++);
 
+      // Each ParticleSpec already contains lists of TagClaims, TagChecks,
+      // and Edges that shall be generated for each Particle implementing that
+      // Spec, but which are rooted at the ParticleSpec rather than the
+      // implementing Recipe and Particle. Most of the work of generating the
+      // facts for a particular Particle just comes down to swapping out the
+      // ParticleSpec roots for the corresponding Particle roots. The
+      // instantiation_map describing how that swapping should be done is
+      // populated by iterating over all HandleConnectionProtos and, for each
+      // HandleConnectionSpec and HandleConnection pair indicated, adding
+      // that pair to the instantiation_map.
       absl::flat_hash_map<ir::AccessPathRoot, ir::AccessPathRoot>
           instantiation_map;
       for (const arcs::HandleConnectionProto &connection_proto :
@@ -79,8 +109,16 @@ DatalogFacts DatalogFacts::CreateFromManifestProto(
         raksha::ir::HandleConnectionAccessPathRoot
           instantiated_handle_root(
               recipe_name, particle_name, std::move(handle_spec_name));
+
+        // Set up the map to replace the HandleConnectionSpec root with the
+        // HandleConnection root when instantiating the Particle.
+        instantiation_map.insert({
+          ir::AccessPathRoot(std::move(spec_handle_root)),
+          ir::AccessPathRoot(std::move(instantiated_handle_root)) });
+
         raksha::ir::HandleAccessPathRoot handle_root(
             recipe_name, std::move(handle_name));
+
         CHECK(connection_proto.has_type())
           << "Handle connection with absent type not allowed.";
         std::unique_ptr<types::Type> connection_type =
@@ -90,16 +128,14 @@ DatalogFacts DatalogFacts::CreateFromManifestProto(
 
         // For each Selector path from a root to a leaf, draw dataflow edges
         // to and from the AccessPaths rooted at the handle at at the
-        // particle's handle connection. In the case that the handle
-        // connection is input only, the edge out will be unreachable from
-        // the rest of the graph. In the case that the connection is output
-        // only, the edge in will be unreachable from the rest of the graph.
-        // In the case that the connection is bidirectional, both edges will
-        // be useful. Note that while the additional edges are, in some
-        // sense, "wasted", the worst case is that we draw twice as many
-        // edges as we need to, which doesn't affect asymptotic complexity.
-        // In particular, the dataflow graph remains correct, if not as
-        // minimal as it could be.
+        // particle's handle connection. Because we consider the input/output
+        // behavior of HandleConnectionSpecs while drawing the edges within a
+        // ParticleSpec, we can rely upon that to filter out the unnecessary
+        // edges drawn into and out of a Particle. Note that while the
+        // additional edges are, in some sense, "wasted", the worst case is that
+        // we draw twice as many edges as we need to, which doesn't affect
+        // asymptotic complexity. In particular, the dataflow graph remains
+        // correct, if not as minimal as it could be.
         for (const ir::AccessPathSelectors &selectors :
              ir::AccessPathSelectorsSet::CreateAbslSet(
                  access_path_selectors_set)) {
@@ -112,12 +148,6 @@ DatalogFacts DatalogFacts::CreateFromManifestProto(
           result_edges.push_back(
               ir::Edge(handle_connection_access_path, handle_access_path));
         }
-
-        // Set up the map to replace the HandleConnectionSpec root with the
-        // HandleConnection root.
-        instantiation_map.insert({
-          ir::AccessPathRoot(std::move(spec_handle_root)),
-          ir::AccessPathRoot(std::move(instantiated_handle_root)) });
       }
 
       auto find_res = particle_specs.find(particle_spec_name);
