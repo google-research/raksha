baac077ae482bf815af7618c176a0f9f3ec7b173
Mark W Winterrowd <winterrowd@google.com>
2021-10-21 13:13:39 -0700

    Work around an issue in Kokoro GCP Ubuntu images.

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -5,6 +5,9 @@ set -e
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/raksha"
 
+# Work around an issue in GCP Ubuntu images.
+pyenv init --path
+
 # Install bazel.
 use_bazel.sh latest --quiet 4.2.1
 command -v bazel
