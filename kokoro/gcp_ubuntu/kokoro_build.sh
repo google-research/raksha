8fbd78bddd3807dbb05a13845bb40df168b1d2b5
Mark W Winterrowd <winterrowd@google.com>
2021-10-21 16:28:35 -0700

    Try to copy what tensorflow is doing with their bazelrc config.

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -13,9 +13,7 @@ use_bazel.sh 4.2.1 --quiet
 command -v bazel
 bazel version
 
-BAZEL_OPTS="--cxxopt=-std=c++17 --host_cxxopt=-std=c++17 --cxxopt=-Werror --host_cxxopt=-Werror --cxxopt=-Wall --host_cxxopt=-Wall --cxxopt=-Wno-deprecated-declarations --host_cxxopt=-Wno-deprecated-declarations"
-
-bazel test ${BAZEL_OPTS} //src/...
-bazel build ${BAZEL_OPTS} //third_party/arcs/examples:consume third_party/arcs/proto:manifest_cc_proto
-bazel build ${BAZEL_OPTS} //src/analysis/souffle/tests/arcs_manifest_tests_todo/...
-bazel build ${BAZEL_OPTS} //rust/tools/authorization-logic:authorization_logic
+bazel test //src/...
+bazel build //third_party/arcs/examples:consume third_party/arcs/proto:manifest_cc_proto
+bazel build //src/analysis/souffle/tests/arcs_manifest_tests_todo/...
+bazel build //rust/tools/authorization-logic:authorization_logic
