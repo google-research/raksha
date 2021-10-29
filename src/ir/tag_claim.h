213c31f30c70c61e3a9131eda051df3e3d1cca9e
Gogul Balakrishnan <bgogul@google.com>
2021-10-29 10:05:11 -0700

    Change `downgrade` to `removeTag`. (#200)
    * Change `downgrade` to removeTag.
    
    * Replaced an incorrect use of "removed" with "downgraded".

--- src/ir/tag_claim.h
+++ src/ir/tag_claim.h
@@ -89,7 +89,7 @@ class TagClaim {
     constexpr absl::string_view kClaimTagFormat =
         R"(%s("%s", "%s", "%s").)";
     absl::string_view relation_name =
-        (claim_tag_is_present_) ? "claimHasTag" : "saysDowngrades";
+        (claim_tag_is_present_) ? "claimHasTag" : "saysRemoveTag";
     return absl::StrFormat(
         kClaimTagFormat, relation_name, claiming_particle_name_,
         access_path_.ToString(), tag_);
