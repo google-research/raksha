8e99b8a95292da27aa4213ff7692f8db0b398788
Andrew Ferraiuolo <aferr@google.com>
2021-11-30 11:12:13 +0000

    PR fixup

--- src/ir/auth_logic/ast.h
+++ src/ir/auth_logic/ast.h
@@ -25,22 +25,22 @@
 
 namespace raksha::ir::auth_logic {
 
-class AstPrincipal {
+class Principal {
   public:
-    AstPrincipal(std::string name) : name_(std::move(name)) {}
+    Principal(std::string name) : name_(std::move(name)) {}
   private:
     std::string name_;
 };
 
 // Used to represent whether a predicate is negated or not
-enum AstSign { negated, positive };
+enum Sign { negated, positive };
 
-class AstPredicate {
+class Predicate {
   public:
-    AstPredicate(
+    Predicate(
         std::string name,
         std::vector<std::string> args,
-        AstSign sign)
+        Sign sign)
       : name_(std::move(name)),
         args_(std::move(args)),
         // TODO this surpresses an unused private field error about sign_
@@ -48,33 +48,33 @@ class AstPredicate {
   private:
     std::string name_;
     std::vector<std::string> args_;
-    AstSign sign_;
+    Sign sign_;
 };
 
 // This represents an expression that applies either a predicate or a 
-// "canActAs" to a principal, and it is a sub-term of AstFlatFact
+// "canActAs" to a principal, and it is a sub-term of FlatFact
 //    - pred_name(arg_1, ..., arg_n)           (a predicate)
 //    - principal_1 canActAs principal_2       (a canActAs)
 // To encapsulate which form this takes, it:
 //    - uses factory methods to make either form
 //    - uses a private std::variant member that takes either of these two forms
 //    - uses std::visit to do operations on the actual state
-class AstVerbPhrase {
+class VerbPhrase {
   public:
-    static std::unique_ptr<AstVerbPhrase> 
-    CreateVerbPhrase(AstPredicate pred) {
-      return std::unique_ptr<AstVerbPhrase>(new AstVerbPhrase(std::move(pred)));
+    static std::unique_ptr<VerbPhrase> 
+    CreateVerbPhrase(Predicate pred) {
+      return std::unique_ptr<VerbPhrase>(new VerbPhrase(std::move(pred)));
     }
-    static std::unique_ptr<AstVerbPhrase>
-    CreateVerbPhrase(AstPrincipal right_prin) {
-      return std::unique_ptr<AstVerbPhrase>(new AstVerbPhrase(
+    static std::unique_ptr<VerbPhrase>
+    CreateVerbPhrase(Principal right_prin) {
+      return std::unique_ptr<VerbPhrase>(new VerbPhrase(
             std::move(right_prin)));
     }
   private:
     // The principal variant of the internal form means this represents
     // "canActAs principal".
-    std::variant<AstPredicate, AstPrincipal> internal_form_;
-    AstVerbPhrase(std::variant<AstPredicate, AstPrincipal> internal_form)
+    std::variant<Predicate, Principal> internal_form_;
+    VerbPhrase(std::variant<Predicate, Principal> internal_form)
       : internal_form_(std::move(internal_form)) {}
 };
 
@@ -83,119 +83,119 @@ class AstVerbPhrase {
 //   - <predicate>                (* just a predicate *)
 // A similar variant-based pattern is used for this as for VerbPhrase. Here an 
 // inner struct is used for the first form (principal applied to verb_phrase).
-class AstFlatFact {
+class FlatFact {
   public:
-    static std::unique_ptr<AstFlatFact>
-    CreateFlatFact(AstPrincipal prin, AstVerbPhrase verb_phrase) {
-      return std::unique_ptr<AstFlatFact>(new AstFlatFact(
+    static std::unique_ptr<FlatFact>
+    CreateFlatFact(Principal prin, VerbPhrase verb_phrase) {
+      return std::unique_ptr<FlatFact>(new FlatFact(
             VerbPhraseFlatFact(prin, verb_phrase)));
     }
-    static std::unique_ptr<AstFlatFact>
-    CreateFlatFact(AstPredicate prin) {
-      return std::unique_ptr<AstFlatFact>(new AstFlatFact(std::move(prin)));
+    static std::unique_ptr<FlatFact>
+    CreateFlatFact(Predicate prin) {
+      return std::unique_ptr<FlatFact>(new FlatFact(std::move(prin)));
     }
   private:
-    typedef struct VerbPhraseFlatFact {
-      AstPrincipal prin;
-      AstVerbPhrase verb_phrase;
-      VerbPhraseFlatFact(AstPrincipal prin, AstVerbPhrase verb_phrase)
+    struct VerbPhraseFlatFact {
+      Principal prin;
+      VerbPhrase verb_phrase;
+      VerbPhraseFlatFact(Principal prin, VerbPhrase verb_phrase)
         : prin(std::move(prin)),
           verb_phrase(std::move(verb_phrase)) {}
-    } VerbPhraseFlatFact;
-    std::variant<VerbPhraseFlatFact, AstPredicate> internal_form_;
-    AstFlatFact(std::variant<VerbPhraseFlatFact, AstPredicate> internal_form)
+    };
+    std::variant<VerbPhraseFlatFact, Predicate> internal_form_;
+    FlatFact(std::variant<VerbPhraseFlatFact, Predicate> internal_form)
         : internal_form_(std::move(internal_form)) {}
 };
 
 // This is the AST node for facts which have two forms:
 //  - <flat_fact>
 //  - <principal> canSay <flat_fact>
-class AstFact {
+class Fact {
   public:
-    static std::unique_ptr<AstFact>
-    CreateFact(AstFlatFact flat_fact) {
-      return std::unique_ptr<AstFact>(new AstFact(std::move(flat_fact)));
+    static std::unique_ptr<Fact>
+    CreateFact(FlatFact flat_fact) {
+      return std::unique_ptr<Fact>(new Fact(std::move(flat_fact)));
     }
-    static std::unique_ptr<AstFact>
-    CreateFact(AstPrincipal prin, AstFact* fact) {
-      return std::unique_ptr<AstFact>(new AstFact(CanSayFact(std::move(prin),
+    static std::unique_ptr<Fact>
+    CreateFact(Principal prin, Fact* fact) {
+      return std::unique_ptr<Fact>(new Fact(CanSayFact(std::move(prin),
             std::move(fact))));
     }
   private:
-    typedef struct CanSayFact {
-      AstPrincipal prin;
-      AstFact *fact;
-      CanSayFact(AstPrincipal prin, AstFact* fact)
+    struct CanSayFact {
+      Principal prin;
+      Fact *fact;
+      CanSayFact(Principal prin, Fact* fact)
         : prin(std::move(prin)),
           fact(fact) {}
-    } CanSayFact;
-    std::variant<AstFlatFact, CanSayFact> internal_form_;
-    AstFact(std::variant<AstFlatFact, CanSayFact> internal_form)
+    };
+    std::variant<FlatFact, CanSayFact> internal_form_;
+    Fact(std::variant<FlatFact, CanSayFact> internal_form)
       : internal_form_(std::move(internal_form)) {};
 };
 
-// AST Node for AstAssertion (this is a sub-expression of SaysAstAssertion) 
+// AST Node for Assertion (this is a sub-expression of SaysAssertion) 
 // which has two forms:
 //  - <fact>                                      (unconditional)
 //  - <fact> :- <flat_fact_1> ... <flat_fact_n>   (conditional)
-class AstAssertion {
+class Assertion {
   public:
-    static std::unique_ptr<AstAssertion>
-    CreateAssertion(AstFact fact) {
-      return std::unique_ptr<AstAssertion>(new AstAssertion(std::move(fact)));
+    static std::unique_ptr<Assertion>
+    CreateAssertion(Fact fact) {
+      return std::unique_ptr<Assertion>(new Assertion(std::move(fact)));
     }
-    static std::unique_ptr<AstAssertion>
-    CreateAssertion(AstFact lhs,
-        std::vector<AstFlatFact> rhs) {
-      return std::unique_ptr<AstAssertion>(new AstAssertion(
+    static std::unique_ptr<Assertion>
+    CreateAssertion(Fact lhs,
+        std::vector<FlatFact> rhs) {
+      return std::unique_ptr<Assertion>(new Assertion(
             CondAssertion(std::move(lhs), std::move(rhs))));
     }
   private:
-    typedef struct CondAssertion {
-      AstFact lhs;
-      std::vector<AstFlatFact> rhs;
-      CondAssertion(AstFact lhs, std::vector<AstFlatFact> rhs)
+    struct CondAssertion {
+      Fact lhs;
+      std::vector<FlatFact> rhs;
+      CondAssertion(Fact lhs, std::vector<FlatFact> rhs)
         : lhs(std::move(lhs)), rhs(std::move(rhs)) {}
-    } CondAssertion;
-    std::variant<AstFact, CondAssertion> internal_form_;
-    AstAssertion(std::variant<AstFact, CondAssertion> internal_form)
+    };
+    std::variant<Fact, CondAssertion> internal_form_;
+    Assertion(std::variant<Fact, CondAssertion> internal_form)
       : internal_form_(std::move(internal_form)) {};
 };
 
 // AST node for saysAssertion, which prepends an assertion with:
 // "<principal> says"
-class AstSaysAssertion {
+class SaysAssertion {
   public:
-    AstSaysAssertion(AstPrincipal prin, std::vector<AstAssertion> assertions)
+    SaysAssertion(Principal prin, std::vector<Assertion> assertions)
       : prin_(std::move(prin)), assertions_(std::move(assertions)) {};
   private:
-    AstPrincipal prin_;
-    std::vector<AstAssertion> assertions_;
+    Principal prin_;
+    std::vector<Assertion> assertions_;
 };
 
 // A query which can be used to test whether "<principal> says <fact>" is true
 // given the rules and base facts.
-class AstQuery {
+class Query {
   public:
-    AstQuery(std::string name, AstPrincipal prin, AstFact fact)
+    Query(std::string name, Principal prin, Fact fact)
       : name_(std::move(name)),
         prin_(std::move(prin)),
         fact_(std::move(fact)) {}
   private:
     std::string name_;
-    AstPrincipal prin_;
-    AstFact fact_;
+    Principal prin_;
+    Fact fact_;
 };
 
-class AstProgram {
+class Program {
   public:
-    AstProgram(std::vector<AstSaysAssertion> says_assertions,
-        std::vector<AstQuery> queries)
-      : says_assertions_(says_assertions),
-        queries_(queries) {}; 
+    Program(std::vector<SaysAssertion> says_assertions,
+        std::vector<Query> queries)
+      : says_assertions_(std::move(says_assertions)),
+        queries_(std::move(queries)) {}; 
   private:
-    std::vector<AstSaysAssertion> says_assertions_;
-    std::vector<AstQuery> queries_;
+    std::vector<SaysAssertion> says_assertions_;
+    std::vector<Query> queries_;
 };
 
 } // namespace raksha::ir::auth_logic
