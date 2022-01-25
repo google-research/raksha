b91f0c3516b096b1b462bf526f777658f2672955
Gogul Balakrishnan <bgogul@google.com>
2022-01-25 00:42:13 -0800

    Include gtest-death-test in common/testing/gtest.h

--- src/common/testing/gtest.h
+++ src/common/testing/gtest.h
@@ -1,8 +1,9 @@
 #ifndef COMMON_TESTING_TEST_H_
 #define COMMON_TESTING_TEST_H_
 
+#include <gtest/gtest-death-test.h>
 #include <gtest/gtest.h>
 // gmock is necessary to get the full set of matchers to use with gtest.
 #include <gmock/gmock.h>
 
-#endif // COMMON_TESTING_TEST_H_
+#endif  // COMMON_TESTING_TEST_H_
