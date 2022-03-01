ab9998a711822aaac36bf93c11f844611fb331a5
Andrew Ferraiuolo <aferr@google.com>
2022-03-01 18:56:26 +0000

    Commit WIP to work on other PR

--- rust/tools/authorization-logic/src/souffle/universe_translation.rs
+++ rust/tools/authorization-logic/src/souffle/universe_translation.rs
@@ -34,7 +34,7 @@
 //! It should come before the translation in `lowering_ast_datalog.rs`.
 
 use crate::ast::*;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 // Determine if a function argument or principal name is a constant or 
 // return false if this name is a variable. In the ast, all arguments are
@@ -42,7 +42,7 @@ use std::collections::HashMap;
 // they are constants if and only if they are wrapped in quotes. A better
 // way to represent this in the future might be to add an AST node for
 // arguments with varibales and constants as two possible forms.
-fn is_name_constant(arg: &String) -> bool {
+fn is_name_constant(arg: &str) -> bool {
     return arg.contains("\"")
 }
 
@@ -67,27 +67,206 @@ fn universe_condition_principal(principal: &AstPrincipal) -> Option<AstPredicate
 fn type_to_universe_name(ast_type: &AstType) -> String {
     match ast_type {
         AstType::NumberType => "isNumber".to_string(),
-        AstType::SymbolType => "isSymbol".to_string(),
-        AstType::PrincipalType => "isPrincipal".to_string()
+        AstType::PrincipalType => "isPrincipal".to_string(),
+        AstType::CustomType { type_name } => format!("is{}", type_name.clone())
     }
 }
 
-fn universe_declarations() -> Vec<AstTypeDeclaration> {
-    [
-        ("isNumber", AstType::NumberType),
-        ("isSymbol", AstType::SymbolType),
-        ("isPrincipal", AstType::PrincipalType)
-    ].iter().map(|(rel, typ)| AstTypeDeclaration {
-        predicate_name: rel.to_string(),
+fn universe_declarations(rel_decls: &Vec<AstRelationDeclaration>)
+    -> Vec<AstRelationDeclaration> {
+    // Set of all custom type names
+    let custom_type_names = rel_decls.iter()
+        .map(|rel_decl| rel_decl.arg_typings.clone())
+        .flatten()
+        .map(|(name, typ)| typ)
+        .filter_map(|type_| match type_ {
+            AstType::CustomType { type_name } => Some(type_name.clone()),
+            _ => None
+        })
+        .collect::<HashSet<_>>();
+
+    // Vector of pairs of the form (universe_relation_name, type of argument)
+    let mut universe_type_pairs = custom_type_names.iter()
+        .map(|type_name|
+             (format!("is{}", type_name),
+                 AstType::CustomType{ type_name: type_name.to_string() }))
+        .collect::<Vec<_>>();
+    universe_type_pairs.extend([
+        ("isNumber".to_string(), AstType::NumberType),
+        ("isPrincipal".to_string(), AstType::PrincipalType)
+    ]);
+    
+    universe_type_pairs.iter().map(|(rel_name, typ)| AstRelationDeclaration {
+        predicate_name: rel_name.to_string(),
         is_attribute: false,
         arg_typings: vec![("x".to_string(), typ.clone())]
     })
     .collect::<Vec<_>>()
 }
 
+//-----------------------------------------------------------------------------
+// Populate Constant Type Environment & Type Checking
+//-----------------------------------------------------------------------------
+// These functions populate a mapping from constants (which may either be constant
+// principals or other constant values applied as arguments to predicates) to types.
+// This environment is needed to generate facts that put the constants into 
+// universe relations; we need to know which universe relation to put each one in.
+
+// TODO these functions should be refactored into a visitor. These are probably
+// the first really good use-case for a visitor since this is the first
+// functionality that traverses the AST in a way that is not highly order-dependent.
+// TODO it might also be useful to move this into its own separate pass.
+
+// This attempts to add a typing for principals and predicate arguments if they
+// are constant. It does nothing if it is applied to an `arg_name` that is not
+// constant. If there is no typing for this constant, it adds this typing to
+// the environment. If the same typing already exists in the environment,
+// it does nothing. If the constant already has a different typing in the
+// environment, throw an error.
+fn add_typing(constant_type_environment: &mut HashMap<String, AstType>,
+                   arg_name: &str, typ: AstType) {
+    if(is_name_constant(arg_name)) {
+        let arg_name_string = arg_name.to_string();
+        match constant_type_environment.get(&arg_name_string) {
+            None => {
+                constant_type_environment.insert(arg_name_string, typ);
+            },
+            Some(old_typ) => { 
+                if(old_typ != &typ) {
+                    // Type errors could be improved by getting line numbers.
+                    panic!("type error: {} was used with distinct types {:?} and {:?}",
+                           arg_name, old_typ, typ);
+                }
+            }
+        }
+    }
+}
+
+
+fn populate_constant_type_environment_prog(
+    relation_type_environment: &HashMap<String, AstRelationDeclaration>,
+    prog: &AstProgram) -> HashMap<String, AstType> {
+    let mut constant_type_environment = HashMap::new();
+
+    // Add typings for constant speakers in says_assertions
+    for says_assertion in &prog.assertions
+    {
+        add_typing(&mut constant_type_environment,
+                   &says_assertion.prin.name,
+                   AstType::PrincipalType);
+    }
+
+    // Add typings for constants appearing in assertions
+    for assertion in (prog.assertions.iter()
+        .map(|says_assertion| &says_assertion.assertions)
+        .flatten()) {
+        populate_constant_type_environment_assertion(
+            &relation_type_environment, 
+            &mut constant_type_environment, 
+            &assertion);
+    }
+
+    constant_type_environment
+}
+
+fn populate_constant_type_environment_assertion(
+    relation_type_environment: &HashMap<String, AstRelationDeclaration>,
+    constant_type_environment: &mut HashMap<String, AstType>,
+    assertion: &AstAssertion) {
+    match assertion {
+        AstAssertion::AstFactAssertion { f: fact } => {
+            populate_constant_type_environment_fact(
+                &relation_type_environment,
+                constant_type_environment,
+                fact);
+        }
+        AstAssertion::AstCondAssertion { lhs, rhs } => {
+            populate_constant_type_environment_fact(
+                &relation_type_environment,
+                constant_type_environment,
+                lhs);
+            for flat_fact in rhs {
+                populate_constant_type_environment_flat_fact(
+                    &relation_type_environment,
+                    constant_type_environment,
+                    flat_fact);
+            }
+        }
+    }
+}
+
+fn populate_constant_type_environment_fact(
+    relation_type_environment: &HashMap<String, AstRelationDeclaration>,
+    constant_type_environment: &mut HashMap<String, AstType>,
+    fact: &AstFact) {
+    match fact {
+        AstFact::AstFlatFactFact { f: flat_fact } => {
+            populate_constant_type_environment_flat_fact(
+                relation_type_environment,
+                constant_type_environment,
+                flat_fact);
+        },
+        AstFact::AstCanSayFact { p: speaker, f: fact } => {
+            add_typing(constant_type_environment, &speaker.name,
+                       AstType::PrincipalType);
+            populate_constant_type_environment_fact(
+                relation_type_environment,
+                constant_type_environment,
+                fact);
+        }
+    }
+}
+
+fn populate_constant_type_environment_flat_fact(
+        relation_type_environment: &HashMap<String, AstRelationDeclaration>,
+        constant_type_environment: &mut HashMap<String, AstType>,
+        flat_fact: &AstFlatFact) {
+        match flat_fact {
+            AstFlatFact::AstPrinFact { p: principal, v: verb_phrase } => {
+                add_typing(constant_type_environment, &principal.name,
+                           AstType::PrincipalType);
+                match verb_phrase {
+                    AstVerbPhrase::AstPredPhrase { p : predicate } => {
+                        populate_constant_type_environment_predicate(
+                            relation_type_environment,
+                            constant_type_environment,
+                            predicate);
+                    }
+                    AstVerbPhrase::AstCanActPhrase { p : as_principal } => {
+                        add_typing(constant_type_environment, &as_principal.name,
+                                   AstType::PrincipalType);
+                    }
+                }
+            }
+            AstFlatFact::AstPredFact { p: predicate } => {
+                populate_constant_type_environment_predicate(
+                    relation_type_environment,
+                    constant_type_environment,
+                    predicate);
+            }
+        }
+}
+    
+fn populate_constant_type_environment_predicate(
+    relation_type_environment: &HashMap<String, AstRelationDeclaration>,
+    constant_type_environment: &mut HashMap<String, AstType>,
+    predicate: &AstPredicate) {
+    for (arg, typ) in (&relation_type_environment
+                       .get(&predicate.name)
+                       .expect(&format!("couldn't find type for {:#?} in {:#?}\n",
+                               &predicate.name,
+                               &relation_type_environment))
+                       .arg_typings) {
+        add_typing(constant_type_environment, arg, typ.clone());
+    }
+}
+
+
 pub struct UniverseHandlingPass {
-    // mapping from predicate names to predicate typings
-    type_environment: HashMap<String, AstTypeDeclaration>
+    // mapping from relation names to relation typings
+    relation_type_environment: HashMap<String, AstRelationDeclaration>,
+    // maping from constant names to constant typings
+    constant_type_environment: HashMap<String, AstType>
 }
 
 impl UniverseHandlingPass {
@@ -100,19 +279,116 @@ impl UniverseHandlingPass {
                 universe_handling_pass.add_universe_conditions_says_assertion(x)
             })
             .collect();
-        new_prog.type_declarations.append(&mut universe_declarations());
+        new_prog.relation_declarations.append(&mut universe_declarations(
+                &prog.relation_declarations));
         new_prog
     }
 
     fn new(prog: &AstProgram) -> UniverseHandlingPass {
-        UniverseHandlingPass {
-            type_environment: (&prog.type_declarations)
+        let rel_typ_env = (&prog.relation_declarations)
                 .iter()
                 .map(|decl| (decl.predicate_name.clone(), decl.clone()))
-                .collect()
+                .collect();
+
+        UniverseHandlingPass {
+            constant_type_environment: 
+                populate_constant_type_environment_prog(
+                    &rel_typ_env, prog),
+            relation_type_environment: rel_typ_env
         }
     }
 
+    //-----------------------------------------------------------------------------
+    // Add Universe Defining Facts
+    //-----------------------------------------------------------------------------
+    // These functions find all the constant principals, and all the constant predicate
+    // arguments, and emits facts that populate the universes.
+    // fn add_universe_defining_says_assertions(self, prog: &AstProgram) -> AstProgram {
+    //     let constant_speakers = prog.assertions.iter()
+    //         .map(|says_assertion| says_assertion.prin)
+    //         .filter(|prin| is_name_constant(prin.name))
+    //         .collect();
+    //     let universe_defining_facts = !iproduct(constant_speakers(prog),
+    //         constant_subexpressions(prog)).iter()
+    //         .map(|(speaker, expr)|
+    //              self.universe_defining_says_relation(&speaker, &expr)
+    //         );
+    // }
+
+    fn universe_defining_says_assertion(self, speaker: &AstPrincipal,
+                                        expr: &str) -> AstSaysAssertion {
+        AstSaysAssertion {
+            prin: speaker.clone(),
+            assertions: vec![ AstAssertion::AstFactAssertion {
+                f: AstFact::AstFlatFactFact {
+                    f: AstFlatFact::AstPredFact {
+                        p: AstPredicate {
+                            sign: Sign::Positive,
+                            name: type_to_universe_name(
+                                self.constant_type_environment.get(expr).unwrap()),
+                            args: vec![expr.to_string()]
+                        }
+                    }
+                }
+            }],
+            export_file: None
+        }
+    }
+
+    fn constant_subexpressions(prog: &AstProgram) -> HashSet<String> {
+        HashSet::new()
+        // prog.assertions.iter()
+        //     .map(|says_assertion| says_assertion.assertions)
+        //     .flatten()
+        //     .map(|assertion| constant_subexpressions(assertion))
+        //     .collect::<HashSet<_>>()
+    }
+
+    /*
+    fn constant_subexpressions_assertion(assertion: &AstAssertion) -> HashSet<String> {
+        match assertion {
+            AstAssertion::AstFactAssertion { f: fact } => 
+                constant_subexpressions_fact(&fact),
+            AstAssertion::AstCondAssertion { lhs, rhs } => {
+                let mut ret = constant_subexpressions_fact(&lhs);
+                ret.extend(
+                    rhs.iter()
+                    .map(|flat_fact| constant_subexpressions_flat_fact(flat_fact))
+                    .flatten());
+                ret
+            }
+        }
+    }
+
+    fn constant_subexpressions_fact(fact: &AstFact) -> HashSet<String> {
+        match fact {
+            AstFact::AstFlatFactFact { f: flat_fact } => {
+                constant_subexpressions_flat_fact(flat_fact)
+            },
+            AstFact::AstCanSayFact { p: speaker, f: fact } => {
+                // TODO
+            }
+        }
+    }
+    
+    fn constant_subexpressions_flat_fact(flat_fact: &AstFlatFact)
+        -> HashSet<String> {
+            // TODO
+            // match flat_fact {
+            //     AstFlatFact::AstPrinFact { p: principal, v: verb_prhase } => 
+            // }
+    }
+    */
+
+    //-----------------------------------------------------------------------------
+    // Add rules for delegating universe membership
+    //-----------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    // Add Grounding Conditions
+    //-----------------------------------------------------------------------------
+    // For rules and facts that would contain otherwise ungrounded variables,
+    // add universe relations to the RHS to make these assertions grounded.
     fn add_universe_conditions_says_assertion(&self,
         says_assertion: &AstSaysAssertion)  -> AstSaysAssertion {
         AstSaysAssertion {
@@ -125,18 +401,25 @@ impl UniverseHandlingPass {
     }
 
     // For ungrounded variables appearing in the LHS, extend the RHS
-    // with prediates that put them in the universe relations
+    // with prediates that put them in the universe relations.
+    // TODO at the moment, this conservatively adds a universe condition for all variables
+    // on the LHS rather than just the ungrounded ones. Extend this to
+    // capture just the ungrounded variables.
     fn add_universe_conditions_assertion(&self, assertion: &AstAssertion)
         -> AstAssertion {
             match assertion {
                 AstAssertion::AstFactAssertion { f: fact } => {
-                    let new_rhs = self.universe_conditions_fact(&fact);
-                    AstAssertion::AstCondAssertion {
-                        lhs: fact.clone(),
-                        rhs: self.universe_conditions_fact(&fact)
-                            .iter()
-                            .map(|pred| AstFlatFact::AstPredFact{p: pred.clone()})
-                            .collect()
+                    let new_rhs_predicates = self.universe_conditions_fact(&fact);
+                    if new_rhs_predicates.len() > 0 {
+                        AstAssertion::AstCondAssertion {
+                            lhs: fact.clone(),
+                            rhs: new_rhs_predicates
+                                .iter()
+                                .map(|pred| AstFlatFact::AstPredFact{p: pred.clone()})
+                                .collect()
+                        }
+                    } else {
+                        assertion.clone()
                     }
                 }
                 AstAssertion::AstCondAssertion { lhs, rhs } => {
@@ -207,22 +490,34 @@ impl UniverseHandlingPass {
 
     fn universe_conditions_predicate(&self, predicate: &AstPredicate)
         -> Vec<AstPredicate> {
-            predicate.args.iter()
-                .map(|arg| self.universe_condition_arg(&predicate.name, arg))
+            predicate.args.iter().enumerate()
+                .map(|(pos, arg)| self.universe_condition_arg(
+                        &predicate.name, arg, pos))
                 .flatten()
                 .collect()
     }
 
-    fn universe_condition_arg(&self, predicate_name: &String, argument: &String)
+    fn universe_condition_arg(&self, predicate_name: &str, argument: &str, position: usize)
         -> Option<AstPredicate> {
-            let predicate_decl = self.type_environment.get(predicate_name).expect(
+            let predicate_decl = self.relation_type_environment.get(predicate_name).expect(
                 &format!("UniverseHandlingPass couldn't find a declaration for {}",
                         &predicate_name));
-            let arg_to_type_map: HashMap<_, _> = predicate_decl.arg_typings
-                .clone()
+            // Creates a map from positions in the relation declaration to the
+            // type of the parameter at those positions. For example,
+            // `.decl foo(x: SomeThing, y: SomeOtherThing)`
+            // would yield the map
+            // `[(1, CustomType{ SomeThing }),
+            // (2, CustomType { SomeOtherThing })]`
+            let positional_arg_to_type_map: HashMap<_, _> = 
+                predicate_decl.arg_typings
+                .iter()
+                .map(|(param_name, typ)| typ)
                 .into_iter()
+                .enumerate()
                 .collect();
-            let arg_type = arg_to_type_map.get(argument).unwrap();
+            let arg_type = positional_arg_to_type_map
+                .get(&position)
+                .unwrap();
             if !is_name_constant(argument) {
                 Some(AstPredicate {
                     sign: Sign::Positive,
