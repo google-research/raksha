7637225252756f3f4d4b9c5c22d76392ade1e96b
Gogul Balakrishnan <bgogul@google.com>
2022-03-23 02:23:38 -0700

    Add a sql::LiteralOp class  (#476)
    Closes #476
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google-research/raksha/pull/476 from google-research:literal_op@bgogul 05f7d00229d8c0bf367e05e175db8af8926357ba
    PiperOrigin-RevId: 436684360

---
+++ src/frontends/sql/ops/literal_op.cc
@@ -0,0 +1,49 @@
+//-----------------------------------------------------------------------------
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------------
+
+#include "src/frontends/sql/ops/literal_op.h"
+
+#include <memory>
+
+#include "src/ir/attributes/attribute.h"
+#include "src/ir/attributes/string_attribute.h"
+#include "src/ir/ir_context.h"
+#include "src/ir/module.h"
+#include "src/ir/value.h"
+
+namespace raksha::frontends::sql {
+
+std::unique_ptr<LiteralOp> LiteralOp::Create(const ir::Block* parent_block,
+                                             const ir::IRContext& context,
+                                             absl::string_view literal) {
+  return std::make_unique<LiteralOp>(
+      parent_block, *CHECK_NOTNULL(SqlOp::GetOperator<LiteralOp>(context)),
+      ir::NamedAttributeMap(
+          {{std::string(kLiteralStringAttrName),
+            ir::Attribute::Create<ir::StringAttribute>(literal)}}),
+      ir::NamedValueMap({}),
+      /*impl_module=*/nullptr);
+}
+
+absl::string_view LiteralOp::GetLiteralString() const {
+  auto find_result = attributes().find(kLiteralStringAttrName);
+  CHECK(find_result != attributes().end());
+  auto literal = find_result->second;
+  auto string_literal = CHECK_NOTNULL(literal.GetIf<ir::StringAttribute>());
+  return string_literal->value();
+}
+
+}  // namespace raksha::frontends::sql
---
+++ src/frontends/sql/ops/literal_op_test.cc
@@ -0,0 +1,44 @@
+//-----------------------------------------------------------------------------
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------------
+#include "src/frontends/sql/ops/literal_op.h"
+
+#include <algorithm>
+
+#include "src/common/testing/gtest.h"
+#include "src/frontends/sql/ops/sql_op.h"
+#include "src/ir/attributes/attribute.h"
+#include "src/ir/attributes/string_attribute.h"
+
+namespace raksha::frontends::sql {
+namespace {
+
+TEST(LiteralOpTest, FactoryMethodCreatesCorrectOperation) {
+  ir::IRContext context;
+  SqlOp::RegisterOperator<LiteralOp>(context);
+
+  std::unique_ptr<ir::Operation> op =
+      LiteralOp::Create(nullptr, context, "hello");
+  EXPECT_EQ(op->op().name(), OpTraits<LiteralOp>::kName);
+  EXPECT_EQ(op->parent(), nullptr);
+  EXPECT_TRUE(op->inputs().empty());
+  EXPECT_THAT(op->attributes(),
+              testing::UnorderedElementsAre(testing::Pair(
+                  std::string(LiteralOp::kLiteralStringAttrName),
+                  ir::Attribute::Create<ir::StringAttribute>("hello"))));
+}
+
+}  // namespace
+}  // namespace raksha::frontends::sql
