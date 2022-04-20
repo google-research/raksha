97e55de0f7b9dc1abfd3577bdc24393101251a0b
Mark W Winterrowd <winterrowd@google.com>
2022-04-19 22:50:19 -0700

    Fix virtual destructor issue with Values and ADTs.

--- src/backends/policy_enforcement/souffle/value.h
+++ src/backends/policy_enforcement/souffle/value.h
@@ -34,14 +34,21 @@ namespace raksha::backends::policy_enforcement::souffle {
 class Value {
  public:
   virtual std::string ToDatalogString() const = 0;
+};
 
- private:
+// This is the common supertype of any Souffle value type that can
+// participate in an ADT. This is all Values except Records. Values that can
+// participate in an ADT must have a virtual destructor.
+class AdtMemberValue : public Value {
+ public:
+  virtual ~AdtMemberValue() {}
 };
 
 // Corresponds to Souffle's `number` type.
-class Number : public Value {
+class Number : public AdtMemberValue {
  public:
   explicit Number(int64_t value) : number_value_(value) {}
+  virtual ~Number() {}
 
   std::string ToDatalogString() const override {
     return std::to_string(number_value_);
@@ -52,9 +59,10 @@ class Number : public Value {
 };
 
 // Corresponds to Souffle's `symbol` type.
-class Symbol : public Value {
+class Symbol : public AdtMemberValue {
  public:
   explicit Symbol(absl::string_view value) : symbol_value_(value) {}
+  virtual ~Symbol() {}
 
   std::string ToDatalogString() const override {
     return absl::StrFormat(R"("%s")", symbol_value_);
@@ -91,102 +99,24 @@ class Record : public Value {
   std::unique_ptr<std::tuple<RecordFieldValueTypes...>> record_arguments_;
 };
 
-// An ADT branch with the branch name and argument values erased from the
-// template, instead living as dynamic entries in the `branch_name_` and
-// `args_` fields. This is necessary because a declaration that asks for an
-// ADT will accept any of the branches of that ADT, so static typing
-// information is lost at the declaration site. This keeps its args as a
-// vector of unique pointers to `Value`s; this is necessary because ADTs can
-// have recursive definitions (it is common for branches of an ADT to have
-// members also of that ADT type) so we need to use pointers to arguments to
-// get a well-formed layout.
 template <const char *kAdtName>
-class ErasedAdtBranch {
+class Adt : public AdtMemberValue {
  public:
-  template <class... AdtBranchArgs>
-  ErasedAdtBranch(absl::string_view branch_name, AdtBranchArgs &&...branch_args)
-      : branch_name_(branch_name),
-        args_(MakeUniquePtrValueVectorFromValues(
-            std::forward<AdtBranchArgs>(branch_args)...)) {}
+  explicit Adt(absl::string_view branch_name) : branch_name_(branch_name) {}
 
   std::string ToDatalogString() const {
     return absl::StrFormat(
         "$%s{%s}", branch_name_,
-        absl::StrJoin(args_, ", ", [](std::string *str, const auto &arg) {
+        absl::StrJoin(members_, ", ", [](std::string *str, const auto &arg) {
           absl::StrAppend(str, arg->ToDatalogString());
         }));
   }
 
+ protected:
+  std::vector<std::unique_ptr<AdtMemberValue>> members_;
+
  private:
-  static void MakeUniquePtrValueVectorFromValuesInner(
-      std::vector<std::unique_ptr<Value>> &result) {}
-
-  template <class ValueArg, class... ValueArgs>
-  static void MakeUniquePtrValueVectorFromValuesInner(
-      std::vector<std::unique_ptr<Value>> &result, ValueArg &&value,
-      ValueArgs &&...values) {
-    result.push_back(std::make_unique<ValueArg>(std::forward<ValueArg>(value)));
-    MakeUniquePtrValueVectorFromValuesInner(result,
-                                            std::forward<ValueArgs>(values)...);
-  }
-
-  // This helper is used to unwind the variadic parameter pack used to hold the
-  // statically arguments on an incoming `AdtBranch` and turn them into a
-  // `vector` of arguments, where the only static type information known is
-  // that they descend from `Value`.
-  template <class... ValueArgs>
-  static std::vector<std::unique_ptr<Value>> MakeUniquePtrValueVectorFromValues(
-      ValueArgs &&...values) {
-    std::vector<std::unique_ptr<Value>> result;
-    MakeUniquePtrValueVectorFromValuesInner(result,
-                                            std::forward<ValueArgs>(values)...);
-    return result;
-  }
-
   absl::string_view branch_name_;
-  std::vector<std::unique_ptr<Value>> args_;
-};
-
-// An ADT branch with full static type information. This is basically used to
-// get a friendly, type-safe constructor around an `ErasedAdtBranch`.
-template <const char *kAdtName, const char *kAdtBranchName,
-          class... AdtBranchArgumentTypes>
-class AdtBranch {
- public:
-  explicit AdtBranch(AdtBranchArgumentTypes &&...args)
-      : erased_branch_(kAdtBranchName,
-                       std::forward<AdtBranchArgumentTypes>(args)...) {}
-
-  std::string ToDatalogString() const {
-    return erased_branch_.ToDatalogString();
-  }
-
-  template <const char *kAdtFriendName>
-  friend class Adt;
-
- private:
-  ErasedAdtBranch<kAdtName> erased_branch_;
-};
-
-// An ADT, which may hold any branch belonging to that ADT. Because of the
-// type system tricks we are playing, we express this as a wrapper holding a
-// single `ErasedAdtBranch`, which must statically indicate that it belongs
-// to this ADT.
-template <const char *kAdtName>
-class Adt : public Value {
- public:
-  Adt(ErasedAdtBranch<kAdtName> branch) : branch_(std::move(branch)) {}
-
-  template <const char *kAdtBranchName, class... AdtBranchArgumentTypes>
-  Adt(AdtBranch<kAdtName, kAdtBranchName, AdtBranchArgumentTypes...> branch)
-      : branch_(std::move(branch.erased_branch_)) {}
-
-  std::string ToDatalogString() const override {
-    return branch_.ToDatalogString();
-  }
-
- private:
-  ErasedAdtBranch<kAdtName> branch_;
 };
 
 }  // namespace raksha::backends::policy_enforcement::souffle
