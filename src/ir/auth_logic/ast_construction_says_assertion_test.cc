1671ad35ee019f54966f04b90c42c4709f153d03
Harsha Mandadi <harshamandadi@google.com>
2022-04-22 14:09:53 -0700

    Resolving merge conflicts and review changes

---
+++ src/ir/auth_logic/ast_construction_says_assertion_test.cc
@@ -0,0 +1,89 @@
+//-------------------------------------------------------------------------------
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-------------------------------------------------------------------------------
+
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <optional>
+#include <stdexcept>
+
+#include "src/common/testing/gtest.h"
+#include "src/common/utils/map_iter.h"
+#include "src/ir/auth_logic/ast.h"
+#include "src/ir/auth_logic/ast_construction.h"
+#include "src/ir/auth_logic/ast_string.h"
+
+namespace raksha::ir::auth_logic {
+
+struct SaysAssertionTestData {
+  std::string input_text;
+  std::string principal;
+  std::vector<std::string> assertions;
+};
+
+class AstConstructionTest
+    : public testing::TestWithParam<SaysAssertionTestData> {};
+
+class SaysAssertionAstConstructionTest
+    : public testing::TestWithParam<SaysAssertionTestData> {};
+
+TEST_P(SaysAssertionAstConstructionTest, SimpleTestWithSaysAssertionProgram) {
+  const auto& [input_text, principal, assertions] = GetParam();
+  Program prog_out = ParseProgram(input_text);
+  ASSERT_EQ(prog_out.says_assertions().size(), 1);
+  const SaysAssertion& says_assertion = prog_out.says_assertions().front();
+  EXPECT_EQ(says_assertion.principal().name(), principal);
+  EXPECT_EQ(says_assertion.assertions().size(), assertions.size());
+  std::vector<std::string> assertion_string = utils::MapIter<std::string>(
+      says_assertion.assertions(),
+      [](Assertion assertion) { return ToString(assertion); });
+  EXPECT_THAT(assertion_string,
+              ::testing::UnorderedElementsAreArray(assertions));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    SaysAssertionAstConstructionTest, SaysAssertionAstConstructionTest,
+    testing::Values(
+        SaysAssertionTestData({R"("UserA" says { currTimeIs(2022).
+                               mayA(NotifyA) :- currTimeIs > 2021. })",
+                               R"("UserA")",
+                               {"currTimeIs(2022)",
+                                "mayA(NotifyA) :- currTimeIs > 2021"}}),
+        SaysAssertionTestData({R"("UserB" says { currTimeIs(2020).
+                               mayB(NotifyB) :- currTimeIs < 2021. })",
+                               R"("UserB")",
+                               {"currTimeIs(2020)",
+                                "mayB(NotifyB) :- currTimeIs < 2021"}}),
+        SaysAssertionTestData({R"("UserC" says { currTimeIs(2021).
+                               mayC(NotifyC) :- currTimeIs = 2021). })",
+                               R"("UserC")",
+                               {"currTimeIs(2021)",
+                                "mayC(NotifyC) :- currTimeIs = 2021"}}),
+        SaysAssertionTestData({R"("UserC" says { currTimeIs(2021).
+                               mayC(NotifyC) :- currTimeIs >= 2021). })",
+                               R"("UserC")",
+                               {"currTimeIs(2021)",
+                                "mayC(NotifyC) :- currTimeIs >= 2021"}}),
+        SaysAssertionTestData({R"("UserC" says { currTimeIs(2021).
+                               mayC(NotifyC) :- currTimeIs <= 2021). })",
+                               R"("UserC")",
+                               {"currTimeIs(2021)",
+                                "mayC(NotifyC) :- currTimeIs <= 2021"}})
+
+            ));
+
+}  // namespace raksha::ir::auth_logic
---
+++ src/ir/auth_logic/ast_string.h
@@ -0,0 +1,90 @@
+
+//-------------------------------------------------------------------------------
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-------------------------------------------------------------------------------
+#include <string>
+#include <utility>
+#include <variant>
+#include <vector>
+
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_join.h"
+#include "src/common/utils/overloaded.h"
+#include "src/ir/auth_logic/ast.h"
+#include "src/ir/datalog/program.h"
+
+namespace raksha::ir::auth_logic {
+std::vector<std::string> binary_operations = {"<", ">", "=", "!=", "<=", ">="};
+
+std::string ToString(datalog::Predicate predicate) {
+  if (std::find(binary_operations.begin(), binary_operations.end(),
+                predicate.name()) != binary_operations.end()) {
+    return absl::StrCat(predicate.args().front(), " ", predicate.name(), " ",
+                        predicate.args().back());
+  }
+  return absl::StrCat(predicate.sign() == datalog::Sign::kNegated ? "!" : "",
+                      predicate.name(), "(",
+                      absl::StrJoin(predicate.args(), ", "), ")");
+}
+
+std::string ToString(BaseFact basefact) {
+  return std::visit(
+      raksha::utils::overloaded{
+          [](datalog::Predicate predicate) { return ToString(predicate); },
+          [](Attribute attribute) {
+            return absl::StrJoin(
+                {attribute.principal().name(), ToString(attribute.predicate())},
+                " ");
+          },
+          [](CanActAs can_act_as) {
+            return absl::StrJoin({can_act_as.left_principal().name(),
+                                  can_act_as.right_principal().name()},
+                                 " canActAs ");
+          }},
+      basefact.GetValue());
+}
+
+std::string ToString(Fact fact) {
+  if (fact.delegation_chain().empty()) {
+    return ToString(fact.base_fact());
+  }
+  std::string cansay_string = "";
+  for (const Principal& delegatees : fact.delegation_chain()) {
+    cansay_string =
+        absl::StrJoin({delegatees.name(), cansay_string}, " canSay ");
+  }
+  return absl::StrJoin({cansay_string, ToString(fact.base_fact())}, " ");
+}
+
+std::string ToString(ConditionalAssertion conditional_assertion) {
+  std::string lhs = ToString(conditional_assertion.lhs());
+  std::vector<std::string> rhs;
+  for (const BaseFact& base_fact : conditional_assertion.rhs()) {
+    rhs.push_back(ToString(base_fact));
+  }
+  return absl::StrJoin({lhs, absl::StrJoin(rhs, ",")}, " :- ");
+}
+
+std::string ToString(Assertion assertion) {
+  return std::visit(
+      raksha::utils::overloaded{[](Fact fact) { return ToString(fact); },
+                                [](ConditionalAssertion conditional_assertion) {
+                                  return ToString(conditional_assertion);
+                                }},
+      assertion.GetValue());
+}
+
+}  // namespace raksha::ir::auth_logic
