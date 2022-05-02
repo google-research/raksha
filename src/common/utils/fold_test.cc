3f65d71b7aa5dbc254daee35ddd3a258a294637c
Mark W Winterrowd <winterrowd@google.com>
2022-05-02 14:14:17 -0700

    Add a `fold` and `rfold` function to our common utils.
    C++ provides `std::accumulate`, which provides the classic higher-order
    function `fold`. Unfortunately, `std::accumulate` copies its accumulator
    until C++20, making it unsuitable for use with accumulators that have no
    copy constructor. To work around this, this change adds a `fold` and an
    `rfold` function, which can handle an uncopyable accumulator. If we
    advance Raksha to C++20, we can just make these wrappers around
    `std::accumulate`.

---
+++ src/common/utils/fold_test.cc
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------------
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------------
+
+#include "src/common/utils/fold.h"
+
+#include "absl/strings/str_join.h"
+#include "src/common/testing/gtest.h"
+
+namespace raksha::common::utils {
+
+struct IntVecAndSum {
+  std::vector<int> int_vec;
+  int sum;
+};
+
+class SumFoldTest : public testing::TestWithParam<IntVecAndSum> {};
+
+TEST_P(SumFoldTest, SumFoldTest) {
+  const auto &[vec, expected_result] = GetParam();
+  auto sum_fn = [](int sum_so_far, int num) { return sum_so_far + num; };
+  EXPECT_EQ(fold(vec, 0, sum_fn), expected_result);
+  EXPECT_EQ(rfold(vec, 0, sum_fn), expected_result);
+}
+
+static const IntVecAndSum kIntVecsAndSum[] = {
+    {.int_vec = {}, .sum = 0},
+    {.int_vec = {1}, .sum = 1},
+    {.int_vec = {1, 2, 3, 4}, .sum = 10},
+    {.int_vec = {-1, 5, 2, -8}, .sum = -2}};
+
+INSTANTIATE_TEST_SUITE_P(SumFoldTest, SumFoldTest,
+                         testing::ValuesIn(kIntVecsAndSum));
+
+class ConcatFoldTest : public testing::TestWithParam<std::vector<std::string>> {
+};
+
+TEST_P(ConcatFoldTest, ConcatFoldTest) {
+  const std::vector<std::string> &string_vec = GetParam();
+  auto concat_fn = [](std::string concat_so_far, const std::string &input) {
+    // Yes, this is bad form in general, but the quadraticness shouldn't matter
+    // for a test.
+    return concat_so_far + input;
+  };
+  EXPECT_EQ(fold(string_vec, std::string(""), concat_fn),
+            absl::StrJoin(string_vec, ""));
+  EXPECT_EQ(rfold(string_vec, std::string(""), concat_fn),
+            absl::StrJoin(string_vec.rbegin(), string_vec.rend(), ""));
+}
+
+static const std::vector<std::string> kSampleStringVecs[] = {
+    {}, {"x"}, {"hello", "world"}, {"a", "b", "c", "d", "e"}};
+
+INSTANTIATE_TEST_SUITE_P(ConcatFoldTest, ConcatFoldTest,
+                         testing::ValuesIn(kSampleStringVecs));
+
+// Box and unbox some strings via `fold`. This mainly tests that we can handle
+// an uncopyable accumulator (ie, the vector of unique pointers that we build
+// up) and an uncopyable input vector.
+class BoxFoldTest : public testing::TestWithParam<std::vector<std::string>> {};
+
+TEST_P(BoxFoldTest, BoxFoldTest) {
+  const std::vector<std::string> &string_vec = GetParam();
+  auto box_fn = [](std::vector<std::unique_ptr<std::string>> boxed_so_far,
+                   const std::string &input) {
+    boxed_so_far.push_back(std::make_unique<std::string>(input));
+    return boxed_so_far;
+  };
+  std::vector<std::unique_ptr<std::string>> boxed_strings =
+      fold(string_vec, std::vector<std::unique_ptr<std::string>>(), box_fn);
+  auto unbox_fn = [](std::vector<std::string> unboxed_so_far,
+                     const std::unique_ptr<std::string> &to_unbox) {
+    unboxed_so_far.push_back(*to_unbox);
+    return unboxed_so_far;
+  };
+  std::vector<std::string> unboxed_strings =
+      fold(boxed_strings, std::vector<std::string>(), unbox_fn);
+  EXPECT_THAT(unboxed_strings, testing::ElementsAreArray(string_vec));
+}
+
+INSTANTIATE_TEST_SUITE_P(BoxFoldTest, BoxFoldTest,
+                         testing::ValuesIn(kSampleStringVecs));
+
+}  // namespace raksha::common::utils
