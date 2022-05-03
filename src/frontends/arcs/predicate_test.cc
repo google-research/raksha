7336ba5d28720936e681a5d39c982a9b46d7c325
Gogul Balakrishnan <bgogul@google.com>
2022-05-03 10:57:41 -0700

    Move `xform_to_datalog` and arcs code in `src/ir` to `frontends/arcs` (#537)
    This move is done to unblock the following tasks:
      - Get the old datalog path out of the way to avoid confusion with the new datalog lowering.
      - Cleaning up the ir directory so that we can document it.
      - Migrate arcs to the new ir.
    
    The data structures related to `AccessPathSelectors` had to be left in `src/ir` because it is still used in types. Once we implement a visitor for types, we can move this out as well.
    
    Closes #537
    
    PiperOrigin-RevId: 446236080

---
+++ src/frontends/arcs/predicate_test.cc
@@ -0,0 +1,191 @@
+//-----------------------------------------------------------------------------
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//----------------------------------------------------------------------------
+
+#include "src/frontends/arcs/predicate.h"
+
+#include "absl/strings/str_replace.h"
+#include "absl/strings/substitute.h"
+#include "google/protobuf/text_format.h"
+#include "src/common/testing/gtest.h"
+#include "src/frontends/arcs/datalog_print_context.h"
+#include "src/frontends/arcs/predicate_textproto_to_rule_body_testdata.h"
+#include "src/frontends/arcs/proto/predicate.h"
+#include "third_party/arcs/proto/manifest.pb.h"
+
+namespace raksha::frontends::arcs {
+
+class ToDatalogRuleBodyTest
+    : public testing::TestWithParam<std::tuple<
+          std::tuple<absl::string_view, absl::string_view>, AccessPath>> {
+ public:
+  explicit ToDatalogRuleBodyTest() : access_path_(std::get<1>(GetParam())) {
+    const auto &[textproto, expected_rule_body_format] =
+        std::get<0>(GetParam());
+
+    textproto_ = textproto;
+    expected_rule_body_ = absl::Substitute(expected_rule_body_format,
+                                           access_path_.ToString(), "owner");
+  }
+
+ protected:
+  std::string textproto_;
+  std::string expected_rule_body_;
+  AccessPath access_path_;
+};
+
+TEST_P(ToDatalogRuleBodyTest, ToDatalogRuleBodyTest) {
+  DatalogPrintContext ctxt;
+  ::arcs::InformationFlowLabelProto_Predicate predicate_proto;
+  ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(textproto_,
+                                                            &predicate_proto));
+  std::unique_ptr<Predicate> predicate = proto::Decode(predicate_proto);
+  EXPECT_EQ(predicate->ToDatalogRuleBody(access_path_, ctxt),
+            expected_rule_body_);
+}
+
+// Helper for making AccessPaths.
+static ir::Selector MakeFieldSelector(std::string field_name) {
+  return ir::Selector(ir::FieldSelector(field_name));
+}
+
+// Some sample access path textprotos. They aren't the main point of the test.
+static const AccessPath sample_access_paths[] = {
+    AccessPath(AccessPathRoot(HandleConnectionAccessPathRoot("a", "b", "c")),
+               ir::AccessPathSelectors(
+                   MakeFieldSelector("d"),
+                   ir::AccessPathSelectors(MakeFieldSelector("e"),
+                                           ir::AccessPathSelectors()))),
+    AccessPath(
+        AccessPathRoot(HandleConnectionAccessPathRoot("foo", "bar", "baz")),
+        ir::AccessPathSelectors()),
+    AccessPath(
+        AccessPathRoot(HandleConnectionAccessPathRoot("foo", "bar", "baz")),
+        ir::AccessPathSelectors(MakeFieldSelector("field"),
+                                ir::AccessPathSelectors())),
+};
+
+// Note: look in predicate_textproto_to_rule_body_testdata for
+// predicate_textproto_to_rule_body_format.
+INSTANTIATE_TEST_SUITE_P(
+    ToDatalogRuleBodyTest, ToDatalogRuleBodyTest,
+    testing::Combine(testing::ValuesIn(predicate_textproto_to_rule_body_format),
+                     testing::ValuesIn(sample_access_paths)));
+
+class PredicateEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<const Predicate *, uint64_t>,
+                     std::pair<const Predicate *, uint64_t>>> {
+ protected:
+  PredicateEqTest()
+      : pred1_(std::get<0>(GetParam()).first),
+        pred_num1(std::get<0>(GetParam()).second),
+        pred2_(std::get<1>(GetParam()).first),
+        pred_num2(std::get<1>(GetParam()).second) {}
+
+  const Predicate *pred1_;
+  uint64_t pred_num1;
+  const Predicate *pred2_;
+  uint64_t pred_num2;
+};
+
+// For this test, we create a number of expected-distinct predicates and
+// number them. We make sure that the predicates compare equal only when they
+// have the same number. We produce duplicates that are structurally the same
+// but at different memory addresses to ensure that equality in the case of
+// pointer equality is handled correctly.
+TEST_P(PredicateEqTest, PredicateEqTest) {
+  EXPECT_EQ(*pred1_ == *pred2_, pred_num1 == pred_num2);
+}
+
+static const TagPresence kTag1Present("tag1");
+static const TagPresence kTag2Present("tag2");
+static const TagPresence kTag3Present("tag3");
+
+static const TagPresence kTag1Present2("tag1");
+static const TagPresence kTag2Present2("tag2");
+static const TagPresence kTag3Present2("tag3");
+
+static const And kAndTag1Tag2(std::make_unique<TagPresence>("tag1"),
+                              std::make_unique<TagPresence>("tag2"));
+static const And kAndTag1Tag3(std::make_unique<TagPresence>("tag1"),
+                              std::make_unique<TagPresence>("tag3"));
+static const And kAndTag2Tag3(std::make_unique<TagPresence>("tag2"),
+                              std::make_unique<TagPresence>("tag3"));
+
+static const And kAndTag1Tag2_2(std::make_unique<TagPresence>("tag1"),
+                                std::make_unique<TagPresence>("tag2"));
+static const And kAndTag1Tag3_2(std::make_unique<TagPresence>("tag1"),
+                                std::make_unique<TagPresence>("tag3"));
+static const And kAndTag2Tag3_2(std::make_unique<TagPresence>("tag2"),
+                                std::make_unique<TagPresence>("tag3"));
+
+static const Or kOrTag1Tag2(std::make_unique<TagPresence>("tag1"),
+                            std::make_unique<TagPresence>("tag2"));
+static const Or kOrTag1Tag3(std::make_unique<TagPresence>("tag1"),
+                            std::make_unique<TagPresence>("tag3"));
+static const Or kOrTag2Tag3(std::make_unique<TagPresence>("tag2"),
+                            std::make_unique<TagPresence>("tag3"));
+
+static const Or kOrTag1Tag2_2(std::make_unique<TagPresence>("tag1"),
+                              std::make_unique<TagPresence>("tag2"));
+static const Or kOrTag1Tag3_2(std::make_unique<TagPresence>("tag1"),
+                              std::make_unique<TagPresence>("tag3"));
+static const Or kOrTag2Tag3_2(std::make_unique<TagPresence>("tag2"),
+                              std::make_unique<TagPresence>("tag3"));
+
+static const Implies kImpliesTag1Tag2(std::make_unique<TagPresence>("tag1"),
+                                      std::make_unique<TagPresence>("tag2"));
+static const Implies kImpliesTag1Tag2_2(std::make_unique<TagPresence>("tag1"),
+                                        std::make_unique<TagPresence>("tag2"));
+static const Implies kImpliesTag1Tag3(std::make_unique<TagPresence>("tag1"),
+                                      std::make_unique<TagPresence>("tag3"));
+static const Implies kImpliesTag1Tag3_2(std::make_unique<TagPresence>("tag1"),
+                                        std::make_unique<TagPresence>("tag3"));
+static const Implies kImpliesTag2Tag3(std::make_unique<TagPresence>("tag2"),
+                                      std::make_unique<TagPresence>("tag3"));
+static const Implies kImpliesTag2Tag3_2(std::make_unique<TagPresence>("tag2"),
+                                        std::make_unique<TagPresence>("tag3"));
+
+static const Not kNotTag1(std::make_unique<TagPresence>("tag1"));
+static const Not kNotTag1_2(std::make_unique<TagPresence>("tag1"));
+static const Not kNotTag2(std::make_unique<TagPresence>("tag2"));
+static const Not kNotTag2_2(std::make_unique<TagPresence>("tag2"));
+static const Not kNotTag3(std::make_unique<TagPresence>("tag3"));
+static const Not kNotTag3_2(std::make_unique<TagPresence>("tag3"));
+
+static const std::pair<const Predicate *, uint64_t> example_predicates[] = {
+    {&kTag1Present, 0},      {&kTag1Present2, 0},
+    {&kTag2Present, 1},      {&kTag2Present2, 1},
+    {&kTag3Present, 2},      {&kTag3Present2, 2},
+    {&kAndTag1Tag2, 3},      {&kAndTag1Tag2_2, 3},
+    {&kAndTag1Tag3, 4},      {&kAndTag1Tag3_2, 4},
+    {&kAndTag2Tag3, 5},      {&kAndTag2Tag3_2, 5},
+    {&kOrTag1Tag2, 6},       {&kOrTag1Tag2_2, 6},
+    {&kOrTag1Tag3, 7},       {&kOrTag1Tag3_2, 7},
+    {&kOrTag2Tag3, 8},       {&kOrTag2Tag3_2, 8},
+    {&kImpliesTag1Tag2, 9},  {&kImpliesTag1Tag2_2, 9},
+    {&kImpliesTag1Tag3, 10}, {&kImpliesTag1Tag3_2, 10},
+    {&kImpliesTag2Tag3, 11}, {&kImpliesTag2Tag3_2, 11},
+    {&kNotTag1, 12},         {&kNotTag1_2, 12},
+    {&kNotTag2, 13},         {&kNotTag2_2, 13},
+    {&kNotTag3, 14},         {&kNotTag3_2, 14}};
+
+INSTANTIATE_TEST_SUITE_P(
+    PredicateEqTest, PredicateEqTest,
+    testing::Combine(testing::ValuesIn(example_predicates),
+                     testing::ValuesIn(example_predicates)));
+
+}  // namespace raksha::frontends::arcs
---
+++ src/frontends/arcs/tag_claim.h
@@ -0,0 +1,74 @@
+//-----------------------------------------------------------------------------
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//-----------------------------------------------------------------------------
+
+#ifndef SRC_FRONTENDS_ARCS_TAG_CLAIM_H_
+#define SRC_FRONTENDS_ARCS_TAG_CLAIM_H_
+
+#include "absl/strings/str_format.h"
+#include "src/common/logging/logging.h"
+#include "src/frontends/arcs/access_path.h"
+#include "src/frontends/arcs/datalog_print_context.h"
+#include "src/frontends/arcs/proto/access_path.h"
+
+namespace raksha::frontends::arcs {
+
+// Represents a claim that adds or removes a single tag from a particular
+// access path.
+class TagClaim {
+ public:
+  explicit TagClaim(std::string claiming_particle_name, AccessPath access_path,
+                    bool claim_tag_is_present, std::string tag)
+      : claiming_particle_name_(std::move(claiming_particle_name)),
+        access_path_(std::move(access_path)),
+        claim_tag_is_present_(claim_tag_is_present),
+        tag_(std::move(tag)) {}
+
+  // Produce a string containing a datalog fact for this TagClaim.
+  std::string ToDatalog(DatalogPrintContext &ctxt) const {
+    constexpr absl::string_view kClaimTagFormat =
+        R"(%s("%s", "%s", owner, "%s") :- ownsAccessPath(owner, "%s").)";
+    absl::string_view relation_name =
+        (claim_tag_is_present_) ? "says_hasTag" : "says_removeTag";
+    return absl::StrFormat(
+        kClaimTagFormat, relation_name, claiming_particle_name_,
+        access_path_.ToDatalog(ctxt), tag_, access_path_.ToDatalog(ctxt));
+  }
+
+  bool operator==(const TagClaim &other) const {
+    return (claiming_particle_name_ == other.claiming_particle_name_) &&
+           (access_path_ == other.access_path_) &&
+           (claim_tag_is_present_ == other.claim_tag_is_present_) &&
+           (tag_ == other.tag_);
+  }
+
+  const AccessPath &access_path() const { return access_path_; }
+
+ private:
+  // The name of the particle performing this claim. Important for connecting
+  // the claim to a principal for authorization logic purposes.
+  std::string claiming_particle_name_;
+  // The access path upon which the claim is being made.
+  AccessPath access_path_;
+  // If true, we are claiming that the tag is present. If false, we are
+  // claiming that the tag is absent.
+  bool claim_tag_is_present_;
+  // The tag being claimed.
+  std::string tag_;
+};
+
+}  // namespace raksha::ir
+
+#endif  // SRC_FRONTENDS_ARCS_TAG_CLAIM_H_
