8c9d413714114a1e764bd32f9094680cf3f3a92b
Gogul Balakrishnan <bgogul@google.com>
2022-05-09 13:44:53 -0700

    Fix tests

--- src/ir/datalog/attribute_test.cc
+++ src/ir/datalog/attribute_test.cc
@@ -28,24 +28,24 @@ using testing::ValuesIn;
 
 TEST(AttributeTest, NumberPayloadConstruction) {
   EXPECT_EQ(Attribute::Number(10).ToDatalogString(),
-            "$NumberAttributePayload{10}");
+            "$NumberAttributePayload(10)");
   EXPECT_EQ(Attribute::Number(Number(42)).ToDatalogString(),
-            "$NumberAttributePayload{42}");
+            "$NumberAttributePayload(42)");
 }
 TEST(AttributeTest, StringPayloadConstruction) {
   EXPECT_EQ(Attribute::String("hello world").ToDatalogString(),
-            R"($StringAttributePayload{"hello world"})");
+            R"($StringAttributePayload("hello world"))");
   EXPECT_EQ(Attribute::String(Symbol("42")).ToDatalogString(),
-            R"($StringAttributePayload{"42"})");
+            R"($StringAttributePayload("42"))");
 }
 
 TEST(AttributeTest, AttributeConstructionTest) {
   EXPECT_EQ(
       Attribute("name", Attribute::String("hello world")).ToDatalogString(),
-      R"(["name", $StringAttributePayload{"hello world"}])");
+      R"(["name", $StringAttributePayload("hello world")])");
   EXPECT_EQ(Attribute("answer_for_everything", Attribute::Number(42))
                 .ToDatalogString(),
-            R"(["answer_for_everything", $NumberAttributePayload{42}])");
+            R"(["answer_for_everything", $NumberAttributePayload(42)])");
 }
 
 }  // namespace raksha::ir::datalog
