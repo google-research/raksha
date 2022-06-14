2720afe1d02985f9b0f317b4124ccc9586c5823c
Adriana Sejfia <adrianasejfia@gmail.com>
2022-06-14 07:22:57 +0000

    schema.h changes

--- src/ir/types/schema.h
+++ src/ir/types/schema.h
@@ -8,7 +8,6 @@
 #include "src/ir/access_path_selectors_set.h"
 #include "src/ir/types/type.h"
 
-
 namespace raksha::ir::types {
 
 class Schema {
@@ -24,37 +23,26 @@ class Schema {
   friend class TypeFactory;
 
   //return a string representing the Schema in the following format: 
-  // name: <name or empty>
-  // fields: 
+  // <schema name> {
   //    <field_name>: <field_kind>
-  //... 
+  //    ... 
+  //}
   //only showing the first layer of the schema, e.g. not returning the values of <field_kind>
   //for easier testing, fields are sorted alphabetically based on their name  
-  std::string ToString(){
-    
-    std::string stringValue = absl::StrCat("name: ", name_.value_or(""), "\n", "fields: \n"); 
-
+  std::string ToString() const{
+    std::string schema_to_string = absl::StrCat(name_.value_or(""), "{\n"); 
     std::vector<std::string> fields_to_string; 
-  
-    for (auto& pair : fields_) {
-
-      auto field_name = pair.first; 
-      auto field_kind = pair.second.type_base().kind() == raksha::ir::types::TypeBase::Kind::kPrimitive ? "primitive" : "entity"; 
-
-      fields_to_string.push_back(absl::StrCat("\t", field_name, ": ", field_kind, "\n"));
+    for (auto& [name, kind] : fields_) {
+      auto kind_to_string = kind.type_base().kind() == raksha::ir::types::TypeBase::Kind::kPrimitive ? "primitive" : "entity"; 
+      fields_to_string.push_back(absl::StrCat("\t", name, ": ", kind_to_string, "\n"));
 
     //TODO: decide if we want to further print out fields of EntityType (will be a bit more complex given entity includes schema.h)
     }
 
     //sorting for easier testing 
     sort(fields_to_string.begin(), fields_to_string.end()); 
-
-    for (auto field: fields_to_string){
-        stringValue += field;
-    }
-    return stringValue;  
-      
-
+    absl::StrAppend(&schema_to_string, absl::StrJoin(fields_to_string, "\n"), "}"); 
+    return schema_to_string;  
   }
 
  private:
