f6e789f50c0b623d403c6d0ff66c163bbd50bfe8
Gogul Balakrishnan <bgogul@google.com>
2022-06-23 17:20:03 -0700

    Use  to identify the check_policy_compliance binary in sh test

--- src/backends/policy_engine/souffle/check_policy_compliance_test.sh
+++ src/backends/policy_engine/souffle/check_policy_compliance_test.sh
@@ -22,39 +22,40 @@ FAIL_RESULT="fail"
 function printUsageAndExit() {
   echo "Usage: "
   echo "  check_policy_compliance_test.sh \ "
-  echo "     ${PASS_RESULT}|${FAIL_RESULT} <ir_file> <sql_policy_rules_file>"
+  echo "     <cmd> ${PASS_RESULT}|${FAIL_RESULT} <ir_file> <sql_policy_rules_file>"
   exit 1
 }
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   printUsageAndExit
 fi
 
-EXPECTATION_ARG=$1
-IR_FILE_ARG=$2
-SQL_POLICY_RULES_FILE_ARG=$3
+CMD_ARG=$1
+EXPECTATION_ARG=$2
+IR_FILE_ARG=$3
+SQL_POLICY_RULES_FILE_ARG=$4
 
-if 
+if
   [ "${EXPECTATION_ARG}" != "${PASS_RESULT}" ] &&
   [ "${EXPECTATION_ARG}" != "${FAIL_RESULT}" ];
 then
   printUsageAndExit
 fi
 
-echo "IR File arg is ${IR_FILE_ARG}"
-echo "SQL Policies R File arg is ${SQL_POLICY_RULES_FILE_ARG}"
-
 # A simple script to test the `check_policy_compliance` command line tool.
 ROOT_DIR=${TEST_SRCDIR}/${TEST_WORKSPACE}
-CMD=${ROOT_DIR}/src/backends/policy_engine/souffle/check_policy_compliance
+CMD=${ROOT_DIR}/${CMD_ARG}
 IR_FILE=${ROOT_DIR}/${IR_FILE_ARG}
 SQL_POLICY_RULES_FILE=${ROOT_DIR}/${SQL_POLICY_RULES_FILE_ARG}
 
 $CMD --ir=$IR_FILE --sql_policy_rules=$SQL_POLICY_RULES_FILE
-if [ $? -eq 0 ]; then
+CMD_RESULT=$?
+if [ ${CMD_RESULT} -eq 0 ]; then
   ACTUAL_RESULT="${PASS_RESULT}"
-else 
+elif [ ${CMD_RESULT} -eq 1 ]; then
   ACTUAL_RESULT="${FAIL_RESULT}"
+else 
+  ACTUAL_RESULT="UNKNOWN"
 fi
 if [ "${ACTUAL_RESULT}" != "${EXPECTATION_ARG}" ]; then
   echo "Policy compliance check does not match expectations!"
