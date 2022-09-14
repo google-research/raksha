6b0fd3af0138c130d6243f8f6a7ccbe564dc4396
Andrew Ferraiuolo <aferr@google.com>
2022-09-14 17:36:22 +0100

    Use test pattern

--- src/ir/auth_logic/ast_equality_test.cc
+++ src/ir/auth_logic/ast_equality_test.cc
@@ -61,206 +61,299 @@ TEST(AstEqualityTestSuite, Test7) {
   EXPECT_TRUE(!(p1 == p2));
 }
 
-TEST(AstEqualityTestSuite, PrinTest) {
-  Principal p1("A");
-  Principal p2("A");
-  Principal p3("B");
-  EXPECT_TRUE(p1 == p1);
-  EXPECT_TRUE(p1 == p2);
-  EXPECT_TRUE(p2 == p1);
-  EXPECT_TRUE(!(p1 == p3));
-  EXPECT_TRUE(p1 != p3);
+
+class PrincipalEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<Principal, uint64_t>,
+            std::pair<Principal, uint64_t>>> {
+};
+
+TEST_P(PrincipalEqTest, PrincipalEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, AttributeTest) {
-  Principal prin1("A");
-  Principal prin2("B");
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  datalog::Predicate pred2("foooo", {"bar", "baz"}, datalog::kPositive);
-  Attribute attr1(prin1, pred1);
-  Attribute attr2(prin1, pred1);
-  Attribute attr3(prin2, pred1);
-  Attribute attr4(prin1, pred2);
-  Attribute attr5(prin2, pred2);
-  EXPECT_TRUE(attr1 == attr1);
-  EXPECT_TRUE(attr1 == attr2);
-  EXPECT_TRUE(attr2 == attr1);
-  EXPECT_TRUE(!(attr1 == attr3));
-  EXPECT_TRUE(!(attr1 == attr4));
-  EXPECT_TRUE(!(attr1 == attr5));
-  EXPECT_TRUE(attr1 != attr3);
-  EXPECT_TRUE(attr1 != attr3);
-  EXPECT_TRUE(attr1 != attr3);
+static Principal prinA("A");
+static Principal prinB("B");
+static std::pair<Principal, uint64_t> principalTestVals[] = {
+  {prinA, 0},
+  {prinB, 1}
+};
+
+INSTANTIATE_TEST_SUITE_P(PrincipalEqTest, PrincipalEqTest,
+  testing::Combine(testing::ValuesIn(principalTestVals),
+    testing::ValuesIn(principalTestVals)));
+
+class AttributeEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<Attribute, uint64_t>,
+          std::pair<Attribute, uint64_t>>> {
+};
+
+TEST_P(AttributeEqTest, AttributeEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, CanActAsTest) {
-  Principal prin1("A");
-  Principal prin2("B");
-  Principal prin3("C");
-  CanActAs canActAs1(prin1, prin2);
-  CanActAs canActAs2(prin1, prin2);
-  CanActAs canActAs3(prin1, prin3);
-  CanActAs canActAs4(prin2, prin3);
-  EXPECT_TRUE(canActAs1 == canActAs1);
-  EXPECT_TRUE(canActAs1 == canActAs2);
-  EXPECT_TRUE(canActAs2 == canActAs1);
-  EXPECT_TRUE(!(canActAs1 == canActAs3));
-  EXPECT_TRUE(!(canActAs1 == canActAs4));
-  EXPECT_TRUE(canActAs1 != canActAs3);
-  EXPECT_TRUE(canActAs1 != canActAs4);
+static datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
+static datalog::Predicate pred2("foooo", {"bar", "baz"}, datalog::kPositive);
+
+static  Attribute attr1(prinA, pred1);
+static  Attribute attr2(prinB, pred2);
+static  Attribute attr3(prinA, pred2);
+static  Attribute attr4(prinB, pred1);
+static std::pair<Attribute, uint64_t> attributeTestVals[] = {
+  {attr1, 0},
+  {attr2, 1},
+  {attr3, 2},
+  {attr4, 3},
+};
+
+INSTANTIATE_TEST_SUITE_P(AttributeEqTest, AttributeEqTest,
+  testing::Combine(testing::ValuesIn(attributeTestVals),
+    testing::ValuesIn(attributeTestVals)));
+
+class CanActAsEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<CanActAs, uint64_t>,
+          std::pair<CanActAs, uint64_t>>> {
+};
+
+TEST_P(CanActAsEqTest, CanActAsEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, BaseFactTest) {
-  Principal prin1("A");
-  Principal prin2("B");
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  datalog::Predicate pred2("foooo", {"bar", "baz"}, datalog::kPositive);
-  Attribute attr1(prin1, pred1);
-  Attribute attr2(prin2, pred2);
-  CanActAs canActAs1(prin1, prin1);
-  CanActAs canActAs2(prin1, prin2);
-  BaseFact baseFact1(pred1);
-  BaseFact baseFact2(pred1);
-  BaseFact baseFact3(pred2);
-  BaseFact baseFact4(attr1);
-  BaseFact baseFact5(attr1);
-  BaseFact baseFact6(attr2);
-  BaseFact baseFact7(canActAs1);
-  BaseFact baseFact8(canActAs1);
-  BaseFact baseFact9(canActAs2);
-  EXPECT_TRUE(baseFact1 == baseFact1);
-  EXPECT_TRUE(baseFact1 == baseFact2);
-  EXPECT_TRUE(baseFact4 == baseFact4);
-  EXPECT_TRUE(baseFact4 == baseFact5);
-  EXPECT_TRUE(baseFact7 == baseFact7);
-  EXPECT_TRUE(baseFact7 == baseFact8);
-  EXPECT_TRUE(!(baseFact1 == baseFact3));
-  EXPECT_TRUE(!(baseFact1 == baseFact4));
-  EXPECT_TRUE(!(baseFact1 == baseFact7));
-  EXPECT_TRUE(!(baseFact4 == baseFact6));
-  EXPECT_TRUE(!(baseFact4 == baseFact1));
-  EXPECT_TRUE(!(baseFact4 == baseFact7));
-  EXPECT_TRUE(baseFact1 != baseFact3);
-  EXPECT_TRUE(baseFact1 != baseFact4);
-  EXPECT_TRUE(baseFact1 != baseFact7);
-  EXPECT_TRUE(baseFact4 != baseFact6);
-  EXPECT_TRUE(baseFact4 != baseFact1);
-  EXPECT_TRUE(baseFact4 != baseFact7);
+static Principal prinC("C");
+CanActAs canActAs1(prinA, prinB);
+CanActAs canActAs2(prinA, prinC);
+CanActAs canActAs3(prinB, prinC);
+static std::pair<CanActAs, uint64_t> canActAsTestVals[] = {
+  {canActAs1, 0},
+  {canActAs2, 1},
+  {canActAs3, 2},
+};
+
+INSTANTIATE_TEST_SUITE_P(CanActAsEqTest, CanActAsEqTest,
+  testing::Combine(testing::ValuesIn(canActAsTestVals),
+    testing::ValuesIn(canActAsTestVals)));
+
+class BaseFactEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<BaseFact, uint64_t>,
+          std::pair<BaseFact, uint64_t>>> {
+};
+
+TEST_P(BaseFactEqTest, BaseFactEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, FactTest) {
-  Principal prin1("A");
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  datalog::Predicate pred2("foooo", {"bar", "baz"}, datalog::kPositive);
-  BaseFact baseFact1(pred1);
-  BaseFact baseFact2(pred2);
-  Fact fact1({}, baseFact1);
-  Fact fact2({}, baseFact1);
-  Fact fact3({prin1}, baseFact1);
-  Fact fact4({}, baseFact2);
-  Fact fact5({prin1}, baseFact2);
-  EXPECT_TRUE(fact1 == fact1);
-  EXPECT_TRUE(fact1 == fact2);
-  EXPECT_TRUE(!(fact1 == fact3));
-  EXPECT_TRUE(!(fact1 == fact4));
-  EXPECT_TRUE(!(fact1 == fact5));
-  EXPECT_TRUE(fact1 != fact3);
-  EXPECT_TRUE(fact1 != fact4);
-  EXPECT_TRUE(fact1 != fact5);
+static BaseFact baseFact1(pred1);
+static BaseFact baseFact2(pred2);
+static BaseFact baseFact3(attr1);
+static BaseFact baseFact4(attr2);
+static BaseFact baseFact5(canActAs1);
+static BaseFact baseFact6(canActAs2);
+static std::pair<BaseFact, uint64_t> baseFactTestVals[] = {
+  {baseFact1, 0},
+  {baseFact2, 1},
+  {baseFact3, 2},
+  {baseFact4, 3},
+  {baseFact5, 4},
+  {baseFact6, 5},
+};
+
+INSTANTIATE_TEST_SUITE_P(BaseFactEqTest, BaseFactEqTest,
+  testing::Combine(testing::ValuesIn(baseFactTestVals),
+    testing::ValuesIn(baseFactTestVals)));
+
+class FactEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<Fact, uint64_t>,
+          std::pair<Fact, uint64_t>>> {
+};
+
+TEST_P(FactEqTest, FactEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
+}
+static Fact fact1({}, baseFact1);
+static Fact fact2({prinA}, baseFact1);
+static Fact fact3({}, baseFact2);
+static Fact fact4({prinB}, baseFact2);
+static std::pair<Fact, uint64_t> factTestVals[] = {
+  {fact1, 0},
+  {fact2, 1},
+  {fact3, 2},
+  {fact4, 3},
+};
+
+INSTANTIATE_TEST_SUITE_P(FactEqTest, FactEqTest,
+  testing::Combine(testing::ValuesIn(factTestVals),
+    testing::ValuesIn(factTestVals)));
+
+class ConditionalAssertionEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<ConditionalAssertion, uint64_t>,
+          std::pair<ConditionalAssertion, uint64_t>>> {
+};
+
+TEST_P(ConditionalAssertionEqTest, ConditionalAssertionEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, ConditionalAssertion) {
-  Principal prin1("A");
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  BaseFact baseFact1(pred1);
-  Fact fact1({}, baseFact1);
-  ConditionalAssertion condAssertion1(fact1, {});
-  ConditionalAssertion condAssertion2(fact1, {});
-  ConditionalAssertion condAssertion3(fact1, {baseFact1});
-  ConditionalAssertion condAssertion4(fact1, {baseFact1, baseFact1});
-  EXPECT_TRUE(condAssertion1 == condAssertion1);
-  EXPECT_TRUE(condAssertion1 == condAssertion2);
-  EXPECT_TRUE(!(condAssertion1 == condAssertion3));
-  EXPECT_TRUE(!(condAssertion1 == condAssertion4));
-  EXPECT_TRUE(condAssertion1 != condAssertion3);
-  EXPECT_TRUE(condAssertion1 != condAssertion4);
+static ConditionalAssertion condAssertion1(fact1, {});
+static ConditionalAssertion condAssertion2(fact1, {baseFact1});
+static ConditionalAssertion condAssertion3(fact1, {baseFact1, baseFact1});
+static std::pair<ConditionalAssertion, uint64_t> condAsstTestVals[] = {
+  {condAssertion1, 0},
+  {condAssertion2, 1},
+  {condAssertion3, 2}
+};
+
+INSTANTIATE_TEST_SUITE_P(ConditionalAssertionEqTest, ConditionalAssertionEqTest,
+  testing::Combine(testing::ValuesIn(condAsstTestVals),
+    testing::ValuesIn(condAsstTestVals)));
+
+class AssertionEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<Assertion, uint64_t>,
+          std::pair<Assertion, uint64_t>>> {
+};
+
+TEST_P(AssertionEqTest, AssertionEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, Assertion) {
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  Fact fact1({}, BaseFact(pred1));
-  ConditionalAssertion condAssertion1(fact1, {});
-  Assertion assertion1(fact1);
-  Assertion assertion2(fact1);
-  Assertion assertion3(condAssertion1);
-  Assertion assertion4(condAssertion1);
-  EXPECT_TRUE(assertion1 == assertion1);
-  EXPECT_TRUE(assertion1 == assertion2);
-  EXPECT_TRUE(assertion3 == assertion3);
-  EXPECT_TRUE(assertion3 == assertion4);
-  EXPECT_TRUE(!(assertion1 == assertion3));
-  EXPECT_TRUE(!(assertion4 == assertion2));
-  EXPECT_TRUE(assertion1 != assertion3);
-  EXPECT_TRUE(assertion4 != assertion2);
+Assertion assertion1(fact1);
+Assertion assertion2(condAssertion1);
+static std::pair<Assertion, uint64_t> asstTestVals[] = {
+  {assertion1, 0},
+  {assertion2, 1},
+};
+
+INSTANTIATE_TEST_SUITE_P(AssertionEqTest, AssertionEqTest,
+  testing::Combine(testing::ValuesIn(asstTestVals),
+    testing::ValuesIn(asstTestVals)));
+
+class SaysAssertionEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<SaysAssertion, uint64_t>,
+          std::pair<SaysAssertion, uint64_t>>> {
+};
+
+TEST_P(SaysAssertionEqTest, SaysAssertionEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, SaysAssertion) {
-  Principal prin1("A");
-  Principal prin2("B");
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  Fact fact1({}, BaseFact(pred1));
-  Assertion assertion1(fact1);
-  SaysAssertion saysAssertion1(prin1, {assertion1});
-  SaysAssertion saysAssertion2(prin1, {assertion1});
-  SaysAssertion saysAssertion3(prin2, {assertion1});
-  SaysAssertion saysAssertion4(prin1, {assertion1, assertion1});
-  SaysAssertion saysAssertion5(prin2, {assertion1, assertion1});
-  EXPECT_TRUE(saysAssertion1 == saysAssertion1);
-  EXPECT_TRUE(saysAssertion1 == saysAssertion2);
-  EXPECT_TRUE(!(saysAssertion1 == saysAssertion3));
-  EXPECT_TRUE(!(saysAssertion1 == saysAssertion4));
-  EXPECT_TRUE(!(saysAssertion1 == saysAssertion5));
-  EXPECT_TRUE(saysAssertion1 != saysAssertion3);
-  EXPECT_TRUE(saysAssertion1 != saysAssertion4);
-  EXPECT_TRUE(saysAssertion1 != saysAssertion5);
+static  SaysAssertion saysAssertion1(prinA, {assertion1});
+static  SaysAssertion saysAssertion2(prinB, {assertion1});
+static  SaysAssertion saysAssertion3(prinA, {assertion1, assertion1});
+static  SaysAssertion saysAssertion4(prinB, {assertion1, assertion1});
+static std::pair<SaysAssertion, uint64_t> saysAsstTestVals[] = {
+  {saysAssertion1, 0},
+  {saysAssertion2, 1},
+  {saysAssertion3, 2},
+  {saysAssertion4, 3},
+};
+
+INSTANTIATE_TEST_SUITE_P(SaysAssertionEqTest, SaysAssertionEqTest,
+  testing::Combine(testing::ValuesIn(saysAsstTestVals),
+    testing::ValuesIn(saysAsstTestVals)));
+
+class QueryEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<Query, uint64_t>,
+          std::pair<Query, uint64_t>>> {
+};
+
+TEST_P(QueryEqTest, QueryEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, Query) {
-  Principal prin1("A");
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  datalog::Predicate pred2("foo", {"bar", "baz"}, datalog::kPositive);
-  Fact fact1({}, BaseFact(pred1));
-  Fact fact2({}, BaseFact(pred2));
-  Query query1("q", prin1, fact1);
-  Query query2("q", prin1, fact1);
-  Query query3("qqq", prin1, fact2);
-  EXPECT_TRUE(query1 == query1);
-  EXPECT_TRUE(query1 == query2);
-  EXPECT_TRUE(!(query1 == query3));
-  EXPECT_TRUE(query1 != query3);
+static Query query1("q", prinA, fact1);
+static Query query2("q", prinB, fact1);
+static Query query3("qqq", prinA, fact2);
+static std::pair<Query, uint64_t> queryTestVals[] = {
+  {query1, 0},
+  {query2, 1},
+  {query3, 2},
+};
+
+INSTANTIATE_TEST_SUITE_P(QueryEqTest, QueryEqTest,
+  testing::Combine(testing::ValuesIn(queryTestVals),
+    testing::ValuesIn(queryTestVals)));
+
+class ProgramEqTest
+    : public testing::TestWithParam<
+          std::tuple<std::pair<Program, uint64_t>,
+          std::pair<Program, uint64_t>>> {
+};
+
+TEST_P(ProgramEqTest, ProgramEqTest) {
+  auto &[obj_and_eq_class1, obj_and_eq_class2] = GetParam();
+  auto &[obj1, eq_class1] = obj_and_eq_class1;
+  auto &[obj2, eq_class2] = obj_and_eq_class2;
+
+  EXPECT_EQ(obj1 == obj2, eq_class1 == eq_class2);
+  EXPECT_EQ(obj1 != obj2, eq_class1 != eq_class2);
 }
 
-TEST(AstEqualityTest, Program) {
-  Principal prin1("A");
-  datalog::Predicate pred1("foo", {"bar", "baz"}, datalog::kPositive);
-  Fact fact1({}, BaseFact(pred1));
-  Assertion assertion1(fact1);
-  SaysAssertion saysAssertion1(prin1, {assertion1});
-  datalog::RelationDeclaration relationDeclaration1("rel1", false, {});
-  Query query1("q", prin1, fact1);
-  Program program1({relationDeclaration1}, {saysAssertion1}, {query1});
-  Program program2({relationDeclaration1}, {saysAssertion1}, {query1});
-  Program program3({}, {}, {});
-  Program program4({relationDeclaration1}, {saysAssertion1}, {query1, query1});
-  Program program5({relationDeclaration1}, {}, {query1, query1});
-  EXPECT_TRUE(program1 == program1);
-  EXPECT_TRUE(program1 == program2);
-  EXPECT_TRUE(!(program1 == program3));
-  EXPECT_TRUE(!(program1 == program4));
-  EXPECT_TRUE(!(program1 == program5));
-  EXPECT_TRUE(program1 != program3);
-  EXPECT_TRUE(program1 != program4);
-  EXPECT_TRUE(program1 != program5);
-}
+static datalog::RelationDeclaration relationDeclaration1("rel1", false, {});
+static Program program1({relationDeclaration1}, {saysAssertion1}, {query1});
+static Program program2({}, {}, {});
+static Program program3({relationDeclaration1}, {saysAssertion1}, {query1, query1});
+static Program program4({relationDeclaration1}, {}, {query1, query1});
+static std::pair<Program, uint64_t> progTestVals[] = {
+  {program1, 0},
+  {program2, 1},
+  {program3, 2},
+  {program4, 3},
+};
+
+INSTANTIATE_TEST_SUITE_P(ProgramEqTest, ProgramEqTest,
+  testing::Combine(testing::ValuesIn(progTestVals),
+    testing::ValuesIn(progTestVals)));
 
 }  // namespace raksha::ir::auth_logic
