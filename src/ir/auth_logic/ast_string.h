1e892b394cc686080d23ec85666d3ea9287d3857
Andrew Ferraiuolo <aferr@google.com>
2022-09-22 17:49:36 +0100

    Finish test

--- src/ir/auth_logic/ast_string.h
+++ src/ir/auth_logic/ast_string.h
@@ -30,11 +30,10 @@ namespace raksha::ir::auth_logic {
 std::vector<std::string> binary_operations = {"<", ">", "=", "!=", "<=", ">="};
 
 using AstNodeVariantType =
-    std::variant<datalog::Predicate, datalog::ArgumentType,
-    datalog::Argument, datalog::RelationDeclaration,
-    Principal, Attribute, CanActAs,
-    BaseFact, Fact, ConditionalAssertion,
-    Assertion, SaysAssertion, Query, Program>;
+    std::variant<datalog::Predicate, datalog::ArgumentType, datalog::Argument,
+                 datalog::RelationDeclaration, Principal, Attribute, CanActAs,
+                 BaseFact, Fact, ConditionalAssertion, Assertion, SaysAssertion,
+                 Query, Program>;
 
 std::string ToString(AstNodeVariantType Node) {
   return std::visit(
@@ -52,7 +51,7 @@ std::string ToString(AstNodeVariantType Node) {
                 ")");
           },
           [](datalog::ArgumentType argumentType) {
-            switch(argumentType.kind()) {
+            switch (argumentType.kind()) {
               case datalog::ArgumentType::Kind::kCustom:
                 return absl::StrCat(argumentType.name(), "::CustomType");
               case datalog::ArgumentType::Kind::kNumber:
@@ -62,46 +61,45 @@ std::string ToString(AstNodeVariantType Node) {
             }
           },
           [](datalog::Argument argument) {
-            return absl::StrCat(
-                argument.argument_name(), " : ",
-                ToString(argument.argument_type()));
+            return absl::StrCat(argument.argument_name(), " : ",
+                                ToString(argument.argument_type()));
           },
           [](datalog::RelationDeclaration relationDeclaration) {
             std::vector<std::string> arg_strings;
             arg_strings.reserve(relationDeclaration.arguments().size());
-            for (const datalog::Argument& arg : relationDeclaration.arguments()) {
+            for (const datalog::Argument& arg :
+                 relationDeclaration.arguments()) {
               arg_strings.push_back(ToString(arg));
             }
-            return absl::StrCat(".decl ",
-              relationDeclaration.is_attribute() ? "attribute " : "",
-              relationDeclaration.relation_name(), "(",
-                                absl::StrJoin(arg_strings, ", "), ")");
+            return absl::StrCat(
+                ".decl ",
+                relationDeclaration.is_attribute() ? "attribute " : "",
+                relationDeclaration.relation_name(), "(",
+                absl::StrJoin(arg_strings, ", "), ")");
           },
-          [](Principal principal) {
-            return principal.name();
-          },
+          [](Principal principal) { return principal.name(); },
           [](Attribute attribute) {
-               return absl::StrCat(ToString(attribute.principal()),
-                " ",
-                ToString(attribute.predicate()));
+            return absl::StrCat(ToString(attribute.principal()), " ",
+                                ToString(attribute.predicate()));
           },
           [](CanActAs canActAs) {
             return absl::StrCat(ToString(canActAs.left_principal()),
-              " canActAs ", ToString(canActAs.right_principal()));
+                                " canActAs ",
+                                ToString(canActAs.right_principal()));
           },
           [](BaseFact basefact) {
             return std::visit(raksha::utils::overloaded{
                                   [](datalog::Predicate predicate) {
-                                    return absl::StrCat("BaseFact(",
-                                      ToString(predicate), ")");
+                                    return absl::StrCat(
+                                        "BaseFact(", ToString(predicate), ")");
                                   },
                                   [](Attribute attribute) {
-                                    return absl::StrCat("BaseFact(",
-                                      ToString(attribute), ")");
+                                    return absl::StrCat(
+                                        "BaseFact(", ToString(attribute), ")");
                                   },
                                   [](CanActAs can_act_as) {
-                                    return absl::StrCat("BaseFact(",
-                                      ToString(can_act_as), ")");
+                                    return absl::StrCat(
+                                        "BaseFact(", ToString(can_act_as), ")");
                                   }},
                               basefact.GetValue());
           },
@@ -112,7 +110,7 @@ std::string ToString(AstNodeVariantType Node) {
                   absl::StrJoin({delegatees.name(), cansay_string}, " canSay ");
             }
             return absl::StrCat("Fact(", cansay_string,
-              ToString(fact.base_fact()), ")");
+                                ToString(fact.base_fact()), ")");
           },
           [](ConditionalAssertion conditional_assertion) {
             std::string lhs = ToString(conditional_assertion.lhs());
@@ -120,14 +118,14 @@ std::string ToString(AstNodeVariantType Node) {
             for (const BaseFact& base_fact : conditional_assertion.rhs()) {
               rhs.push_back(ToString(base_fact));
             }
-            return absl::StrJoin({lhs, absl::StrJoin(rhs, ",")}, " :- ");
+            return absl::StrJoin({lhs, absl::StrJoin(rhs, ", ")}, " :- ");
           },
           [](Assertion assertion) {
             return std::visit(
                 raksha::utils::overloaded{
-                    [](Fact fact) { return ToString(fact); },
+                    [](Fact fact) { return absl::StrCat(ToString(fact), "."); },
                     [](ConditionalAssertion conditional_assertion) {
-                      return ToString(conditional_assertion);
+                      return absl::StrCat(ToString(conditional_assertion), ".");
                     }},
                 assertion.GetValue());
           },
@@ -137,17 +135,19 @@ std::string ToString(AstNodeVariantType Node) {
             for (const Assertion& assertion : saysAssertion.assertions()) {
               assertion_strings.push_back(ToString(assertion));
             }
-            return absl::StrCat(ToString(saysAssertion.principal()), "says {\n",
-                        absl::StrJoin(assertion_strings, "\n"), "}");
+            return absl::StrCat(ToString(saysAssertion.principal()),
+                                " says {\n",
+                                absl::StrJoin(assertion_strings, "\n"), "}");
           },
           [](Query query) {
-            return absl::StrCat("Query(",
-              query.name(), ToString(query.principal()),
-                        ToString(query.fact()), ")");
+            return absl::StrCat("Query(", query.name(), " = ",
+                                ToString(query.principal()), " says ",
+                                ToString(query.fact()), "?)");
           },
           [](Program program) {
             std::vector<std::string> relation_decl_strings;
-            relation_decl_strings.reserve(program.relation_declarations().size());
+            relation_decl_strings.reserve(
+                program.relation_declarations().size());
             for (const datalog::RelationDeclaration& rel_decl :
                  program.relation_declarations()) {
               relation_decl_strings.push_back(ToString(rel_decl));
@@ -155,7 +155,7 @@ std::string ToString(AstNodeVariantType Node) {
             std::vector<std::string> says_assertion_strings;
             says_assertion_strings.reserve(program.says_assertions().size());
             for (const SaysAssertion& says_assertion :
-                program.says_assertions()) {
+                 program.says_assertions()) {
               says_assertion_strings.push_back(ToString(says_assertion));
             }
             std::vector<std::string> query_strings;
---
+++ src/ir/auth_logic/ast_string_test.cc
@@ -0,0 +1,156 @@
+//-------------------------------------------------------------------------------
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//-------------------------------------------------------------------------------
+#include "src/ir/auth_logic/ast_string.h"
+
+#include "src/common/testing/gtest.h"
+#include "src/ir/auth_logic/ast.h"
+#include "src/ir/auth_logic/ast_construction.h"
+#include "src/ir/datalog/program.h"
+
+namespace raksha::ir::auth_logic {
+
+static datalog::Predicate predicate1("predicate1", {"arg1", "arg2"},
+                                     datalog::Sign::kPositive);
+static datalog::Predicate predicate2("predicate2", {"arg1"},
+                                     datalog::Sign::kNegated);
+TEST(PredicateToStringTest, SimplePredicateTest) {
+  EXPECT_EQ(ToString(predicate1), "predicate1(arg1, arg2)");
+  EXPECT_EQ(ToString(predicate2), "!predicate2(arg1)");
+}
+
+static datalog::ArgumentType numberType(datalog::ArgumentType::Kind::kNumber,
+                                        "");
+static datalog::ArgumentType principalType(
+    datalog::ArgumentType::Kind::kPrincipal, "");
+static datalog::ArgumentType customType(datalog::ArgumentType::Kind::kCustom,
+                                        "someProgrammerType");
+TEST(ArgumentTypeToStringTest, SimpleArgumentTypeTest) {
+  EXPECT_EQ(ToString(numberType), "NumberType");
+  EXPECT_EQ(ToString(principalType), "PrincipalType");
+  EXPECT_EQ(ToString(customType), "someProgrammerType::CustomType");
+}
+
+static datalog::Argument argument1("arg1", numberType);
+static datalog::Argument argument2("arg2", principalType);
+static datalog::Argument argument3("arg3", customType);
+TEST(ArgumentToStringTest, SimpleArgumentTest) {
+  EXPECT_EQ(ToString(argument1), "arg1 : NumberType");
+  EXPECT_EQ(ToString(argument2), "arg2 : PrincipalType");
+  EXPECT_EQ(ToString(argument3), "arg3 : someProgrammerType::CustomType");
+}
+
+static datalog::RelationDeclaration relationDeclaration1("rel1", false,
+                                                         {argument1,
+                                                          argument2});
+static datalog::RelationDeclaration relationDeclaration2("rel2", true,
+                                                         {argument3});
+TEST(RelationDeclarationToStringTest, SimpleDeclarationTest) {
+  EXPECT_EQ(ToString(relationDeclaration1),
+            ".decl rel1(arg1 : NumberType, arg2 : PrincipalType)");
+  EXPECT_EQ(ToString(relationDeclaration2),
+            ".decl attribute rel2(arg3 : someProgrammerType::CustomType)");
+}
+
+static Principal principal1("principal1");
+
+TEST(PrincipalToStringTest, SimplePrincipalTest) {
+  EXPECT_EQ(ToString(principal1), "principal1");
+}
+
+static Principal principal2("principal2");
+static Attribute attribute1(principal1, predicate1);
+
+TEST(AttributeToStringTest, SimpleAttributeTest) {
+  EXPECT_EQ(ToString(attribute1), "principal1 predicate1(arg1, arg2)");
+}
+
+static CanActAs canActAs1(principal1, principal2);
+
+TEST(CanActAsToStringTest, SimpleCanActAsTest) {
+  EXPECT_EQ(ToString(canActAs1), "principal1 canActAs principal2");
+}
+
+static BaseFact baseFact1(predicate1);
+static BaseFact baseFact2(attribute1);
+static BaseFact baseFact3(canActAs1);
+TEST(BaseFactToStringTest, SimpleBaseFactTest) {
+  EXPECT_EQ(ToString(baseFact1), "BaseFact(predicate1(arg1, arg2))");
+  EXPECT_EQ(ToString(baseFact2), "BaseFact(principal1 predicate1(arg1, arg2))");
+  EXPECT_EQ(ToString(baseFact3), "BaseFact(principal1 canActAs principal2)");
+}
+
+static Fact fact1({}, baseFact1);
+static Fact fact2({principal1}, baseFact1);
+TEST(FactToStringTest, SimpleFactTest) {
+  EXPECT_EQ(ToString(fact1), "Fact(BaseFact(predicate1(arg1, arg2)))");
+  EXPECT_EQ(ToString(fact2),
+            "Fact(principal1 canSay BaseFact(predicate1(arg1, arg2)))");
+}
+
+static ConditionalAssertion condAssertion1(fact1, {baseFact1});
+static ConditionalAssertion condAssertion2(fact1, {baseFact1, baseFact2});
+TEST(ConditionalAssertionToStringTest, SimpleCondAssertionTest) {
+  EXPECT_EQ(ToString(condAssertion1),
+            "Fact(BaseFact(predicate1(arg1, arg2))) :- "
+            "BaseFact(predicate1(arg1, arg2))");
+  EXPECT_EQ(
+      ToString(condAssertion2),
+      "Fact(BaseFact(predicate1(arg1, arg2))) :- BaseFact(predicate1(arg1, "
+      "arg2)), BaseFact(principal1 predicate1(arg1, arg2))");
+}
+
+static Assertion assertion1(fact1);
+static Assertion assertion2(condAssertion1);
+TEST(AssertionToStringTest, SimpleAssertionTest) {
+  EXPECT_EQ(ToString(assertion1), "Fact(BaseFact(predicate1(arg1, arg2))).");
+  EXPECT_EQ(ToString(assertion2),
+            "Fact(BaseFact(predicate1(arg1, arg2))) :- "
+            "BaseFact(predicate1(arg1, arg2)).");
+}
+
+static SaysAssertion saysAssertion1(principal1, {assertion1});
+static SaysAssertion saysAssertion2(principal2, {assertion1, assertion2});
+TEST(SaysAssertionToStringTest, SimpleSaysAssertionTest) {
+  EXPECT_EQ(ToString(saysAssertion1),
+            "principal1 says {\nFact(BaseFact(predicate1(arg1, arg2))).}");
+  EXPECT_EQ(ToString(saysAssertion2),
+            "principal2 says {\nFact(BaseFact(predicate1(arg1, "
+            "arg2))).\nFact(BaseFact(predicate1(arg1, arg2))) :- "
+            "BaseFact(predicate1(arg1, arg2)).}");
+}
+
+static Query query1("query1", principal1, fact1);
+static Query query2("query2", principal2, fact1);
+TEST(QueryToStringTest, SimpleQueryTest) {
+  EXPECT_EQ(ToString(query1),
+            "Query(query1 = principal1 says Fact(BaseFact(predicate1(arg1, "
+            "arg2)))?)");
+  EXPECT_EQ(ToString(query2),
+            "Query(query2 = principal2 says Fact(BaseFact(predicate1(arg1, "
+            "arg2)))?)");
+}
+
+static Program program1({relationDeclaration1}, {saysAssertion1}, {query1});
+TEST(ProgramToStringTest, SimpleProgramTest) {
+  EXPECT_EQ(
+      ToString(program1),
+      "Program(\n.decl rel1(arg1 : NumberType, arg2 : PrincipalType)principal1 "
+      "says {\nFact(BaseFact(predicate1(arg1, arg2))).}Query(query1 = "
+      "principal1 says Fact(BaseFact(predicate1(arg1, arg2)))?))");
+}
+
+}  // namespace raksha::ir::auth_logic
